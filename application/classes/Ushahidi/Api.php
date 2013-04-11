684bf639a911382fdd1d2dedc419c441913d68fe
Robbie Mackay <rm@robbiemackay.com>
2013-04-12 07:07:06 +1200

    Ensure invalid/non-implemented methods return 405
    * Fix existing 405 exception by setting allow methods
    * Add additional check to see if the method is implemented

--- application/classes/Ushahidi/Api.php
+++ application/classes/Ushahidi/Api.php
@@ -124,14 +124,13 @@ class Ushahidi_Api extends Controller {
 		));
 
 		// Is that a valid method?
-		if ( ! isset($this->_action_map[$this->request->method()]))
+		if ( ! isset($this->_action_map[$this->request->method()]) )
 		{
-			// TODO .. add to the if (maybe??) .. method_exists($this, 'action_'.$this->request->method())
-			throw new Http_Exception_405(array_keys($this->_action_map),
-				'The :method method is not supported. Supported methods are :allowed_methods', array(
-				':method'          => $method,
+			throw HTTP_Exception::factory(405, 'The :method method is not supported. Supported methods are :allowed_methods',array(
+				':method'          => $this->request->method(),
 				':allowed_methods' => implode(', ', array_keys($this->_action_map)),
-			));
+			))
+			->allowed(array_keys($this->_action_map));
 		}
 
 		// Get the basic verb based action..
@@ -151,6 +150,16 @@ class Ushahidi_Api extends Controller {
 		
 		// Override the action
 		$this->request->action($action);
+
+		if (! method_exists($this, 'action_'.$action))
+		{
+			// TODO: filter 'Allow' header to only return implemented methods
+			throw HTTP_Exception::factory(405, 'The :method method is not supported. Supported methods are :allowed_methods',array(
+				':method'          => $this->request->method(),
+				':allowed_methods' => implode(', ', array_keys($this->_action_map)),
+			))
+			->allowed(array_keys($this->_action_map));
+		}
 
 		// Are we be expecting body content as part of the request?
 		if (in_array($this->request->method(), $this->_methods_with_body_content))
