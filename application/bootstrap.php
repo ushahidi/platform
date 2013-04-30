83b922b5007035e2f82ccf17c60d600997c4f165
Robbie Mackay <rm@robbiemackay.com>
2013-05-01 11:42:18 +1200

    Move docs on kohana init options to config/init.php

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -97,18 +97,6 @@ if (Kohana::$environment != Kohana::PRODUCTION)
 
 /**
  * Initialize Kohana, setting the default options.
- *
- * The following options are available:
- *
- * - string   base_url    path, and optionally domain, of your application   NULL
- * - string   index_file  name of your index file, usually "index.php"       index.php
- * - string   charset     internal character set used for input and output   utf-8
- * - string   cache_dir   set the internal cache directory                   APPPATH/cache
- * - integer  cache_life  lifetime, in seconds, of items cached              60
- * - boolean  errors      enable or disable error handling                   TRUE
- * - boolean  profile     enable or disable internal profiling               TRUE
- * - boolean  caching     enable or disable internal caching                 FALSE
- * - boolean  expose      set the X-Powered-By header                        FALSE
  */
 Kohana::init(Kohana::$config->load('init')->as_array());
 
--- application/config/init.php
+++ application/config/init.php
@@ -1,7 +1,22 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
+/**
+ * Initialize Kohana, setting the default options.
+ *
+ * The following options are available:
+ *
+ * - string   base_url    path, and optionally domain, of your application   NULL
+ * - string   index_file  name of your index file, usually "index.php"       index.php
+ * - string   charset     internal character set used for input and output   utf-8
+ * - string   cache_dir   set the internal cache directory                   APPPATH/cache
+ * - integer  cache_life  lifetime, in seconds, of items cached              60
+ * - boolean  errors      enable or disable error handling                   TRUE
+ * - boolean  profile     enable or disable internal profiling               TRUE
+ * - boolean  caching     enable or disable internal caching                 FALSE
+ * - boolean  expose      set the X-Powered-By header                        FALSE
+ */
+
 return array(
-	// @TODO change this to be project-specific
 	'base_url'    => '/',
 	'index_file'  => FALSE,
 	'charset'     => 'utf-8',
