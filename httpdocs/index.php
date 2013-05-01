fc8350572c2af1ade6993061fe9c64863e70dfdb
Robbie Mackay <rm@robbiemackay.com>
2013-05-01 12:33:09 +1200

    Using minimal docroot with no writable dirs
    * Move index and .htaccess to httpdocs folder - always use this as docroot
    * htaccess file lets you change rewrite base with an env variable
    * Update minion script for moved index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -4,36 +4,36 @@
  * The directory in which your application specific resources are located.
  * The application directory must contain the bootstrap.php file.
  *
- * @link http://kohanaframework.org/guide/about.install#application
+ * @see  http://kohanaframework.org/guide/about.install#application
  */
-$application = 'application';
+$application = '../application';
 
 /**
  * The directory in which your modules are located.
  *
- * @link http://kohanaframework.org/guide/about.install#modules
+ * @see  http://kohanaframework.org/guide/about.install#modules
  */
-$modules = 'modules';
+$modules = '../modules';
 
 /**
  * The directory in which the Kohana resources are located. The system
  * directory must contain the classes/kohana.php file.
  *
- * @link http://kohanaframework.org/guide/about.install#system
+ * @see  http://kohanaframework.org/guide/about.install#system
  */
-$system = 'system';
+$system = '../system';
 
 /**
  * The default extension of resource files. If you change this, all resources
  * must be renamed to use the new extension.
  *
- * @link http://kohanaframework.org/guide/about.install#ext
+ * @see  http://kohanaframework.org/guide/about.install#ext
  */
 define('EXT', '.php');
 
 /**
  * Set the PHP error reporting level. If you set this in php.ini, you remove this.
- * @link http://www.php.net/manual/errorfunc.configuration#ini.error-reporting
+ * @see  http://php.net/error_reporting
  *
  * When developing your application, it is highly recommended to enable notices
  * and strict warnings. Enable them by using: E_ALL | E_STRICT
@@ -50,23 +50,29 @@ error_reporting(E_ALL | E_STRICT);
  * End of standard configuration! Changing any of the code below should only be
  * attempted by those with a working knowledge of Kohana internals.
  *
- * @link http://kohanaframework.org/guide/using.configuration
+ * @see  http://kohanaframework.org/guide/using.configuration
  */
 
 // Set the full path to the docroot
 define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
-// Make the application relative to the docroot, for symlink'd index.php
+// Make the application relative to the docroot
 if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
+{
 	$application = DOCROOT.$application;
+}
 
-// Make the modules relative to the docroot, for symlink'd index.php
+// Make the modules relative to the docroot
 if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
+{
 	$modules = DOCROOT.$modules;
+}
 
-// Make the system relative to the docroot, for symlink'd index.php
+// Make the system relative to the docroot
 if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
+{
 	$system = DOCROOT.$system;
+}
 
 // Define the absolute paths for configured directories
 define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
@@ -75,12 +81,6 @@ define('SYSPATH', realpath($system).DIRECTORY_SEPARATOR);
 
 // Clean up the configuration vars
 unset($application, $modules, $system);
-
-if (file_exists('install'.EXT))
-{
-	// Load the installation check
-	return include 'install'.EXT;
-}
 
 /**
  * Define the start time of the application, used for profiling.
@@ -118,4 +118,4 @@ else
 		->execute()
 		->send_headers(TRUE)
 		->body();
-}	
+}
