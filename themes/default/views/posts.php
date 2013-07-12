232773b8503d81f453bfdd3ab30a5843b4896cca
Linda Kamau <kamaulynder@gmail.com>
2013-07-12 21:23:31 +0300

    first take at wiring backbone - posts

---
+++ themes/default/views/posts.php
@@ -0,0 +1,10 @@
+<?php
+require_once "header.php";
+?>
+<script type="text/template" id="posts-template">
+	<p><em>Posts</em></p>
+	<h1><%= title %></h1>
+	<div>
+	    <p><%= content %>
+	<div>
+</script>
