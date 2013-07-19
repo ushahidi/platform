67510bfad5fea00d7d836640fd384c04863f9193
Robbie Mackay <rm@robbiemackay.com>
2013-07-19 21:04:16 +1200

    Add posts layer to MapView using geojson

--- modules/UshahidiUI/media/js/app/views/MapView.js
+++ modules/UshahidiUI/media/js/app/views/MapView.js
@@ -1,16 +1,17 @@
-define(['marionette', 'handlebars', 'App', 'leaflet'],
-	function(Marionette, Handlebars, App, L) {
+define(['marionette', 'handlebars', 'App', 'leaflet', 'App.oauth'],
+	function(Marionette, Handlebars, App, L, OAuth) {
 		// Hack to fix default image url
 		L.Icon.Default.imagePath = "/media/kohana/images";
 		
 		return Marionette.ItemView.extend(
 		{
+			// Blank template just so it renders
 			template : Handlebars.compile(""),
 			initialize: function(options) {
-				
 			},
 			id: 'map',
 			className: 'map',
+			// Use onDomRefresh rather than render() because we need this.$el in the DOM first
 			onDomRefresh: function()
 			{
 				// Don't re-render the map
@@ -30,9 +31,9 @@ define(['marionette', 'handlebars', 'App', 'leaflet'],
 				
 				//this.$el.html(this.template(this.model.attributes));
 				// create a map in the "map" div, set the view to a given place and zoom
-				this.map = L.map(this.$el[0], {
+				var map = this.map = L.map(this.$el[0], {
 					center : new L.LatLng(-36.85, 174.78),
-					zoom : 10,
+					zoom : 5,
 					layers : [osm, minimal, midnight]
 				});
 				
@@ -42,9 +43,15 @@ define(['marionette', 'handlebars', 'App', 'leaflet'],
 					"OSM Mapnik": osm
 				};
 				
+				posts = L.geoJson().addTo(this.map);
+				OAuth.ajax({
+					url : '/api/v2/posts/geojson',
+					success: function (data) { posts.addData(data); }
+				});
+				
 				var overlayMaps = {
-					"Motorways": motorways
-					//"Cities": cities
+					"Motorways": motorways,
+					"Posts": posts
 				};
 				
 				L.control.layers(baseMaps, overlayMaps).addTo(this.map);
