ea4227c5d40f8a28a37e9f07f517a6c1537e5e80
Robbie Mackay <rm@robbiemackay.com>
2013-07-22 15:16:59 +1200

    Connect 'collapsemap' button JS

--- modules/UshahidiUI/media/js/app/views/SearchBarView.js
+++ modules/UshahidiUI/media/js/app/views/SearchBarView.js
@@ -5,5 +5,16 @@ define(['marionette', 'handlebars', 'App', 'text!templates/SearchBar.html'],
 			template : Handlebars.compile(template),
 			initialize: function() {
 			},
+			events : {
+				'click #collapse-map' : 'collapseMap'
+			},
+			// @todo move this into MapView somehow
+			collapseMap : function () {
+				$('#map').toggleClass('map-collapse');
+				console.log('You clicked #collapse-map');
+				this.$('#collapse-tab').text(this.$('#collapse-tab').text() == 'collapse map' ? 'expand map' : 'collapse map');
+				this.$('.leaflet-container .leaflet-control-zoom').toggle();
+				return false;
+			}
 		});
 	}); 
