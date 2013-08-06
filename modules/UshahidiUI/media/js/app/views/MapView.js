0087604c92074a9e2fed31888e8cad4d59b398e7
Robbie Mackay <rm@robbiemackay.com>
2013-08-06 14:43:10 +1200

    Remove extra map layers and center map on markers

--- modules/UshahidiUI/media/js/app/views/MapView.js
+++ modules/UshahidiUI/media/js/app/views/MapView.js
@@ -25,41 +25,37 @@ define(['marionette', 'handlebars', 'App', 'leaflet', 'App.oauth'],
 				var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/528babad266546698317425055510f96/{styleId}/256/{z}/{x}/{y}.png',
 					cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
 				
-				var minimal   = L.tileLayer(cloudmadeUrl, {styleId: 22677, attribution: cloudmadeAttribution}),
-					midnight  = L.tileLayer(cloudmadeUrl, {styleId: 999,   attribution: cloudmadeAttribution}),
-					motorways = L.tileLayer(cloudmadeUrl, {styleId: 46561, attribution: cloudmadeAttribution});
+				var minimal   = L.tileLayer(cloudmadeUrl, {styleId: 22677, attribution: cloudmadeAttribution});
 				
-				//this.$el.html(this.template(this.model.attributes));
 				// create a map in the "map" div, set the view to a given place and zoom
 				var map = this.map = L.map(this.$el[0], {
 					center : new L.LatLng(-36.85, 174.78),
 					zoom : 5,
-					layers : [osm, minimal, midnight]
+					layers : [minimal],
+					scrollWheelZoom : false
 				});
-				
-				var baseMaps = {
-					"Minimal": minimal,
-					"Night View": midnight,
-					"OSM Mapnik": osm
-				};
 				
 				posts = L.geoJson().addTo(this.map);
 				OAuth.ajax({
 					url : '/api/v2/posts/geojson',
-					success: function (data) { posts.addData(data); }
+					success: function (data) { 
+						posts.addData(data);
+						// Center map on post markers
+						//map.fitBounds(posts.getBounds()).setZoom(5);
+						map.panTo(posts.getBounds().getCenter(), { animate: false });
+					}
 				});
 				
+				var baseMaps = {
+					"Minimal": minimal
+				};
+				
 				var overlayMaps = {
-					"Motorways": motorways,
 					"Posts": posts
 				};
 				
 				L.control.layers(baseMaps, overlayMaps).addTo(this.map);
 				
-				// add a marker in the given location, attach some popup content to it and open the popup
-				L.marker([-36.85, 174.78]).addTo(this.map)
-					.bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-					.openPopup();
 				return this;
 			}
 		});
