16fa9887cf98b2674ab48562609f5cae7ebae20a
Robbie Mackay <rm@robbiemackay.com>
2013-08-08 13:16:59 +1200

    Add JS event handlers for main navigation toggle and highlight

--- modules/UshahidiUI/media/js/app/views/HeaderView.js
+++ modules/UshahidiUI/media/js/app/views/HeaderView.js
@@ -11,6 +11,11 @@ define(['marionette', 'handlebars', 'App', 'text!templates/Header.html', 'text!t
 				// @todo update this for real UI
 				//App.vent.on("page:change", this.updateActiveNav, this);
 			},
+			events : {
+				'click .js-views-menu-link' : 'showViewsMenu',
+				'click .js-sets-menu-link' : 'showSetsMenu',
+				'click nav[role="sub-navigation"] ul li' : 'toggleSubNav'
+			},
 			triggers : {
 				'click .js-workspace-panel-button, .js-workspace-panel-button-small' : 'workspace:toggle'
 			},
@@ -18,6 +23,35 @@ define(['marionette', 'handlebars', 'App', 'text!templates/Header.html', 'text!t
 			{
 				this.$('li').removeClass('active');
 				this.$('li[data-page="'+page+'"]').addClass('active')
+			},
+			showViewsMenu : function(e)
+			{
+				// Hide other menu
+				this.$('.js-sets-menu').removeClass('subnav');
+				this.$('.js-sets-menu-link').removeClass('active');
+				// Toggle this menu
+				this.$('.js-views-menu').toggleClass('subnav');
+				this.$('.js-views-menu-link').toggleClass('active');
+				
+				e.preventDefault();
+			},
+			showSetsMenu : function(e)
+			{
+				// Hide other menu
+				this.$('.js-views-menu').removeClass('subnav');
+				this.$('.js-views-menu-link').removeClass('active');
+				// Toggle this menu
+				this.$('.js-sets-menu').toggleClass('subnav');
+				this.$('.js-sets-menu-link').toggleClass('active');
+				
+				e.preventDefault();
+			},
+			toggleSubNav : function (e)
+			{
+				console.log($(e.currentTarget))
+				var $el = this.$(e.currentTarget);
+				$el.siblings().removeClass('active');
+				$el.addClass('active');
 			}
 		});
 	}); 
