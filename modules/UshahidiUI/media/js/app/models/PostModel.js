736abcea5bb460eeb4e4eecdc18ed27b957eb64b
Robbie Mackay <rm@robbiemackay.com>
2013-08-09 15:00:40 +1200

    Tidy up PostModel/Collection and remove pointless Collection.js

--- modules/UshahidiUI/media/js/app/models/PostModel.js
+++ modules/UshahidiUI/media/js/app/models/PostModel.js
@@ -1,19 +1,16 @@
 define(["jquery", "backbone"],
 	function($, Backbone) {
-		// Creates a new Backbone Model class object
 		var PostModel = Backbone.Model.extend(
 		{
 			initialize : function() {
 	
 			},
-	
-			// Default values for all of the Model attributes
+
 			defaults :
 			{
 	
 			},
-	
-			// Get's called automatically by Backbone when the set and/or save methods are called (Add your own logic)
+
 			validate : function(attrs) {
 	
 			},
