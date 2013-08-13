f6f6b6c87d45c61edae0e22441c75efa56e95449
Robbie Mackay <rm@robbiemackay.com>
2013-08-13 16:03:42 +1200

    Add pagination to posts listing
    * Add backbone-pageable plugin and use this to page the Posts collection
    * Add handlebars-pagination helper
    * Make pagination links dynamic
    * Add events for changing page to the PostListView

--- modules/UshahidiUI/media/js/app/collections/PostCollection.js
+++ modules/UshahidiUI/media/js/app/collections/PostCollection.js
@@ -1,13 +1,38 @@
-define(["jquery", "backbone", "models/PostModel", "App"],
-	function($, Backbone, PostModel, App) {
+define(["jquery", "backbone", "models/PostModel", "App", "backbone-pageable"],
+	function($, Backbone, PostModel, App, PageableCollection) {
 		// Creates a new Backbone Collection class object
-		var PostCollection = Backbone.Collection.extend(
+		//var PostCollection = Backbone.Collection.extend(
+		var PostCollection = Backbone.PageableCollection.extend(
 		{
 			model : PostModel,
 			url: App.config.baseurl + "api/v2/posts",
 			// The Ushahidi API returns models under "results".
-			parse: function(response) {
+			parseRecords: function(response) {
 				return response.results;
+			},
+			parseState: function(response) {
+				return this.state;
+			},
+			// Set state params for `Backbone.PageableCollection#state`
+			state: {
+				firstPage: 0,
+				currentPage: 0,
+				pageSize: 3,
+				// Required under server-mode
+				totalRecords: 200,
+				sortKey: 'updated',
+				order: 1 // 1 = desc
+			},
+
+			// Mapping from a `Backbone.PageableCollection#state` key to the
+			// query string parameters accepted by the Ushahidi API.
+			queryParams: {
+				currentPage: null,
+				totalPages: null,
+				totalRecords: null,
+				pageSize: "limit",
+				offset: function () { return this.state.currentPage * this.state.pageSize },
+				sortKey: 'order_by'
 			}
 		});
 	
--- modules/UshahidiUI/media/js/app/views/PostListView.js
+++ modules/UshahidiUI/media/js/app/views/PostListView.js
@@ -1,7 +1,8 @@
-define(['App', 'marionette', 'handlebars', 'views/PostItemView', 'text!templates/PostList.html'],
-	function( App, Marionette, Handlebars, PostItemView, template)
+define(['App', 'marionette', 'handlebars', 'views/PostItemView', 'text!templates/PostList.html', 'text!templates/partials/pagination.html'],
+	function( App, Marionette, Handlebars, PostItemView, template, paginationTemplate)
 	{
-		//CollectionView provides some default rendering logic
+		Handlebars.registerPartial('pagination', paginationTemplate);
+
 		return Marionette.CompositeView.extend( {
 			//Template HTML string
 			template: Handlebars.compile(template),
@@ -16,7 +17,12 @@ define(['App', 'marionette', 'handlebars', 'views/PostItemView', 'text!templates
 			itemViewContainer: '.list-view-posts-list',
 			
 			events: {
-				'click .js-list-view-select-post' : 'showHideBulkActions'
+				'click .js-list-view-select-post' : 'showHideBulkActions',
+				'click .js-page-first' : 'showFirstPage',
+				'click .js-page-next' : 'showNextPage',
+				'click .js-page-prev' : 'showPreviousPage',
+				'click .js-page-last' : 'showLastPage',
+				'click .js-page-change' : 'showPage',
 			},
 			
 			showHideBulkActions : function ()
@@ -32,6 +38,66 @@ define(['App', 'marionette', 'handlebars', 'views/PostItemView', 'text!templates
 					this.$('.js-list-view-bulk-actions').removeClass('visible');
 					this.$('.js-list-view-bulk-actions').addClass('hidden');
 				}
+			},
+
+			serializeData : function ()
+			{
+				var data = { items: this.collection.toJSON() };
+				data = _.extend(data, {
+					pagination: this.collection.state
+				});
+
+				return data;
+			},
+
+			showNextPage : function (e)
+			{
+				e.preventDefault();
+				this.collection.getNextPage();
+				this.updatePagination();
+			},
+			showPreviousPage : function (e)
+			{
+				e.preventDefault();
+				this.collection.getPreviousPage();
+				this.updatePagination();
+			},
+			showFirstPage : function (e)
+			{
+				e.preventDefault();
+				this.collection.getFirstPage();
+				this.updatePagination();
+			},
+			showLastPage : function (e)
+			{
+				e.preventDefault();
+				this.collection.getLastPage();
+				this.updatePagination();
+			},
+			showPage : function (e)
+			{
+				e.preventDefault();
+				var $el = this.$(e.currentTarget);
+				var num = 0;
+				_.each(
+					$el.attr('class').split(" "),
+					function (v) {
+						if (v.indexOf("page-") === 0)
+						{
+							num = v.replace("page-", "");
+						}
+					}
+				);
+				this.collection.getPage(num -1);
+				this.updatePagination();
+			},
+
+			updatePagination: function ()
+			{
+				var template = Handlebars.compile(paginationTemplate);
+				this.$('.pagination').replaceWith(template({
+					pagination: this.collection.state
+				}));
 			}
 		});
 	});
