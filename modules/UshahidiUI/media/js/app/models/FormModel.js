efb414547fee1cec9993d4433bbd9d94f00bd594
Robbie Mackay <rm@robbiemackay.com>
2013-09-20 20:08:05 +1200

    Set urlRoot in Form and Tag models

--- modules/UshahidiUI/media/js/app/models/FormModel.js
+++ modules/UshahidiUI/media/js/app/models/FormModel.js
@@ -1,7 +1,8 @@
-define(['jquery', 'backbone'],
-	function($, Backbone) {
+define(['jquery', 'backbone', 'App'],
+	function($, Backbone, App) {
 		var FormModel = Backbone.Model.extend(
 		{
+			urlRoot: App.config.baseurl + 'api/v2/forms'
 			
 		});
 	
