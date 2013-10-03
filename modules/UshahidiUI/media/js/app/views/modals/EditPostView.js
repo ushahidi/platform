5f8d5458acc1b00fbe0cd82d667d7ffdba5c7a72
Robbie Mackay <rm@robbiemackay.com>
2013-10-03 14:19:29 +1300

    Wire up edit-post js from UI
    Wiring up js to replace title/content with input and textarea fields.
    This is broken.. but we can fix it when we start dealing with real
    content.

--- modules/UshahidiUI/media/js/app/views/modals/EditPostView.js
+++ modules/UshahidiUI/media/js/app/views/modals/EditPostView.js
@@ -4,6 +4,48 @@ define([ 'marionette', 'handlebars', 'text!templates/modals/EditPost.html'],
 		return Marionette.ItemView.extend( {
 			template: Handlebars.compile(template),
 			initialize: function() { },
-			className: 'edit-post'
+			className: 'edit-post',
+			events : {
+				'click .post-title' : 'editPostTitle',
+				'click .post-excerpt' : 'editPostContent'
+			},
+			editPostTitle : function(e)
+			{
+				var $el = this.$(e.currentTarget),
+					$input,
+					text;
+				
+				text = $el.text();
+				$el.text('');
+				$input = Marionette.$('<input />').val(text);
+				
+				$input.appendTo($el).select();
+				$input.blur(
+					function(event)
+					{
+						var newText = $input.val();
+						$el.empty.text(newText);
+					}
+				);
+			},
+			editPostContent : function(e)
+			{
+				var $el = this.$(e.currentTarget),
+					$input,
+					text;
+				
+				text = $el.text();
+				$el.text('');
+				$input = Marionette.$('<textarea />').val(text);
+				
+				$input.appendTo($el).select();
+				$input.blur(
+					function(event)
+					{
+						var newText = $input.val();
+						$el.empty.text(newText);
+					}
+				);
+			}
 		});
 	});
