e1fccb9da6b44e1c2f0ccfae5dff5f5afd60c1a4
Henry Addo <addhen@linuxmail.org>
2013-10-07 20:31:02 +0900

    Add media schema

---
+++ application/classes/Model/Media.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Model for Media
+ *
+ * PHP version 5
+ * LICENSE: This source file is subject to GPLv3 license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/gpl.html
+ * @author     Ushahidi Team <team@ushahidi.com>
+ * @package    Ushahidi - http://source.ushahididev.com
+ * @subpackage Models
+ * @copyright  Ushahidi - http://www.ushahidi.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License Version 3 (GPLv3)
+ */
+
+class Model_Media extends ORM {
+
+
+	// Insert/Update Timestamps
+	protected $_created_column = array('column' => 'created', 'format' => TRUE);
+	protected $_updated_column = array('column' => 'updated', 'format' => TRUE);
+}
