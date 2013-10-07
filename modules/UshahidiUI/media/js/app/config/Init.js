69b90201bed739d5ced07c93f9dabdfd7f49d2f2
Robbie Mackay <rm@robbiemackay.com>
2013-10-07 19:31:53 +1300

    Stop loading backbone.validateAll until we're actually using it

--- modules/UshahidiUI/media/js/app/config/Init.js
+++ modules/UshahidiUI/media/js/app/config/Init.js
@@ -74,7 +74,7 @@ require.config(
 });
 
 // Includes Desktop Specific JavaScript files here (or inside of your Desktop router)
-require(['App', 'routers/AppRouter', 'controllers/Controller', 'jquery', 'backbone.validateAll'],
+require(['App', 'routers/AppRouter', 'controllers/Controller', 'jquery'],
 	function(App, AppRouter, Controller)
 	{
 		App.appRouter = new AppRouter(
