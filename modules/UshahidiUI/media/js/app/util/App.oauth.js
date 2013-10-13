cc55d38253fbd3c4ff629bf616d404ac015ff101
Robbie Mackay <rm@robbiemackay.com>
2013-10-14 09:37:49 +1300

    Make UI request tag and set scopes too

--- modules/UshahidiUI/media/js/app/util/App.oauth.js
+++ modules/UshahidiUI/media/js/app/util/App.oauth.js
@@ -10,7 +10,7 @@ define(['backbone', 'jso2', 'jquery', 'underscore'],
 			authorization: window.config.baseurl + 'oauth/authorize',
 			redirect_uri: window.config.baseurl,
 			scopes: {
-				request: ['posts', 'forms', 'api'],
+				request: ['posts', 'forms', 'api', 'tags', 'sets'],
 				require: ['posts']
 			}
 		});
