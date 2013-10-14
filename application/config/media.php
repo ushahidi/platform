08ba6fb3296abf4965dcf3772004dbacd84c8fd7
Henry Addo <addhen@linuxmail.org>
2013-10-14 12:23:42 +0900

    Add size hight to configuration file

--- application/config/media.php
+++ application/config/media.php
@@ -1,25 +1,21 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	// The public accessible directory where the file will be copied
-	'public_dir' => DOCROOT.'media/<uid>/kohana/<filepath>',
-	// Write the files to the public directory when in production
-	'cache'      => Kohana::$environment === Kohana::PRODUCTION,
-	/**
-	 * The UID for media files.
-	 * This should be unique across the entire project because from a css file
-	 * you want to be able to use relative paths to images.
-	 * Your css file would not know where an image is if it had a UID of its own.
-	 * App versions and repository revisions are good UIDs to use.
-	*/
-	'uid' => NULL, // Replace this later - needs to get passed into app to change paths there too.
-	// 'uid' => "3-0-dev",
+
 	// Where to upload media files eg. images. Take note of the trailing slash
 	'media_upload_dir' => DOCROOT.'media/uploads/',
 
 	// Width to be used to resize the image to a medium size
 	'image_medium_width' => 800,
 
+	// Height to be used to resize the image to a medium size. NULL by default
+	// so the image's aspect ratio is maintain when resizing it.
+	'image_medium_height' => NULL,
+
 	// Width to be used to resize the image to a thumbnail size
 	'image_thumbnail_width' => 70,
+
+	// Height to be used to resize the image to a thumbnail size. NULL by default
+	// so the image's aspect ratio is maintain when resizing it.
+	'image_thumbnail_height' => NULL,
 );
