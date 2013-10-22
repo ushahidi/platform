940198eaa446a00d7cf6c43e7923fda7a8001db2
middle8media <seth@middle8media.com>
2013-10-22 17:06:47 -0400

    Set-Detail first draft
    The commit finishes out first draft of set detail, minus the map and app wiring.
    
    Responsive code and a few bits are still being tweaked.
    
    BC thread - https://basecamp.com/1931265/projects/927232-ushahidi-3-0/messages/16353265-set-detail

--- modules/UshahidiUI/media/js/app/views/SetDetailView.js
+++ modules/UshahidiUI/media/js/app/views/SetDetailView.js
@@ -1,10 +1,8 @@
-define([ 'App', 'marionette', 'handlebars', 'text!templates/SetDetail.html', 'text!templates/partials/set-module.html', 'text!templates/partials/pagination.html',],
-    function( App, Marionette, Handlebars, template, setModuleTemplate, paginationTemplate)
+define([ 'App', 'marionette', 'handlebars', 'text!templates/SetDetail.html', 'text!templates/partials/set-module.html'],
+    function( App, Marionette, Handlebars, template, setModuleTemplate)
 	{
 		// Hacky - make sure we register partials before we call compile
 		Handlebars.registerPartial('set-module', setModuleTemplate);
-		Handlebars.registerPartial('pagination', paginationTemplate);
-
 		return Marionette.ItemView.extend( {
 			template: Handlebars.compile(template),
 			initialize: function() {
