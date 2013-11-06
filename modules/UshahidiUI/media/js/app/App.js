4d93e0a54a8141e676a2bff98c55b830b20295a6
Robbie Mackay <rm@robbiemackay.com>
2013-11-06 14:48:49 +1300

    Remove trailing spaces in App.js

--- modules/UshahidiUI/media/js/app/App.js
+++ modules/UshahidiUI/media/js/app/App.js
@@ -11,23 +11,23 @@ define(['jquery', 'backbone', 'marionette', 'underscore', 'util/App.oauth', 'uti
 	function($, Backbone, Marionette, _, Handlebars, OAuth)
 	{
 		var App = new Backbone.Marionette.Application();
-		
+
 		// Save oauth object in App - just in case
 		App.oauth = OAuth;
-		
+
 		//Organize Application into regions corresponding to DOM elements
 		//Regions can contain views, Layouts, or subregions nested as necessary
 		App.addRegions(
 		{
 			body : 'body'
 		});
-	
+
 		function isMobile()
 		{
 			var ua = (navigator.userAgent || navigator.vendor || window.opera, window, window.document);
 			return (/iPhone|iPod|iPad|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
 		}
-	
+
 		App.mobile = isMobile();
 
 		App.addInitializer(function(/*options*/)
@@ -37,7 +37,7 @@ define(['jquery', 'backbone', 'marionette', 'underscore', 'util/App.oauth', 'uti
 			// Init foundation
 			$(document).foundation();
 		});
-		
+
 		// Global config params
 		App.config = _.extend({
 			baseurl : '/',
@@ -45,6 +45,6 @@ define(['jquery', 'backbone', 'marionette', 'underscore', 'util/App.oauth', 'uti
 			jsdir : '/media/kohana/js',
 			cssdir : '/media/kohana/css'
 		}, window.config);
-		
+
 		return App;
 	});
