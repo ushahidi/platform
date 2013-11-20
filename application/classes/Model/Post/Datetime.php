855a9ebc20a45f2e25817fedcbb2eece03ad16a2
Robbie Mackay <rm@robbiemackay.com>
2013-11-21 10:22:56 +1300

    Refactor loading of post values for the API
    Remove huge union query in Post model. Instead we're iterating over a
    list of post attribute types and loading values through
    $model->load_values_for_post().
    
    Attribute types are now stored in Model_Form_Attribute::_attribute_types.
    This should allow us to extend later and add custom type through
    plugins.
    
    Post_Value:: load_values_for_post() grabs the values for this post
    along with their attribute data. We then iterate over these as we
    did before.
    
    We've also moved the date time formatting into the Post_Datetime
    model get/set functions.

--- application/classes/Model/Post/Datetime.php
+++ application/classes/Model/Post/Datetime.php
@@ -43,4 +43,58 @@ class Model_Post_Datetime extends Model_Post_Value {
 			)
 		));
 	}
+
+	/**
+	 * Handles getting of column
+	 * Overriding this method to handle JS style dates
+	 *
+	 * @param   string $column Column name
+	 * @throws Kohana_Exception
+	 * @return mixed
+	 */
+	public function get($column)
+	{
+		if (array_key_exists($column, $this->_object)
+				AND $column == 'value'
+			)
+		{
+			return ($date = DateTime::createFromFormat('Y-m-d H:i:s', $this->_object[$column]))
+				? $date->format(DateTime::W3C)
+				: $this->_object[$column];
+		}
+
+		return parent::get($column);
+	}
+
+
+	/**
+	 * Handles setting of columns
+	 * Overriding this method to handle JS style dates
+	 *
+	 * @param  string $column Column name
+	 * @param  mixed  $value  Column value
+	 * @throws Kohana_Exception
+	 * @return ORM
+	 */
+	public function set($column, $value)
+	{
+		if ($column == 'value')
+		{
+			// Try to convert W3C format first
+			$date = DateTime::createFromFormat(DateTime::W3C, $value);
+			// If that failed, try standard strtotime
+			if (! $date)
+			{
+				$date = date_create($value);
+			}
+
+			// Output date in MySQL format
+			if ($date)
+			{
+				$value = $date->format('Y-m-d H:i:s');
+			}
+		}
+
+		return parent::set($column, $value);
+	}
 }
