3db8c89ec1b5892a09b7989a11efc97cc631803f
Henry Addo <addhen@linuxmail.org>
2013-11-28 12:55:54 +0900

    Add set collection

--- modules/UshahidiUI/media/js/app/models/SetModel.js
+++ modules/UshahidiUI/media/js/app/models/SetModel.js
@@ -7,10 +7,18 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-define(['jquery', 'backbone', 'App', 'underscore', 'models/UserModel', 'models/FormModel'],
-	function($, Backbone, App, _, UserModel, FormModel) {
-		var PostModel = Backbone.Model.extend(
+define(['jquery', 'backbone', 'App', 'underscore'],
+	function($, Backbone, App) {
+		var SetModel = Backbone.Model.extend(
 		{
-			urlRoot: App.config.apiurl + 'sets',
+			urlRoot: App.config.apiuri + '/sets',
+			id: '',
+			name : '',
+			filter : null,
+			user : null,
+			updated : null
 		});
+
+		return SetModel;
+
 	});
