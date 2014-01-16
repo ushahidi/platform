6b7f6eec5c3a0d7b491aa035390924b2a243a60c
Henry Addo <addhen@linuxmail.org>
2014-01-16 21:23:21 +0900

    Update model to reflect the tags schema

--- modules/UshahidiUI/media/js/app/models/TagModel.js
+++ modules/UshahidiUI/media/js/app/models/TagModel.js
@@ -7,15 +7,88 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-define(['jquery', 'backbone', 'App'],
-	function($, Backbone, App) {
+define(['backbone', 'App'],
+	function(Backbone, App) {
 		var TagModel = Backbone.Model.extend(
 		{
 			urlRoot: App.config.baseurl + App.config.apiuri +'/tags',
-			toString : function ()
+			schema : function ()
 			{
-				return this.get('tag');
+				var schema = {
+					tag: {
+						type: 'Text',
+						title: 'Name',
+						editorAttrs : {
+							placeholder : 'Enter name'
+						}
+					},
+					slug: {
+						type: 'Text',
+						title: 'Slug',
+						editorAttrs : {
+							placeholder : 'Enter a slug'
+						}
+					},
+					description: {
+						type: 'Text',
+						title: 'description',
+						editorAttrs : {
+							placeholder : 'Enter a description'
+						}
+					},
+					category : {
+						type: 'Text',
+						title: 'Category',
+						editorAttrs : {
+							placeholder : 'Enter a category'
+						}
+					},
+					priority : {
+						type: 'Select',
+						title: 'Priority',
+						options: {
+							admin: 'High',
+							user: 'Medium',
+							guest: 'Low'
+						}
+					},
+					color : {
+						type: 'Text',
+						title: 'Color',
+						editorAttrs : {
+							placeholder : 'Enter a color'
+						}
+					}
+				};
+				return schema;
 			}
+			validation : function ()
+			{
+				var rules = {
+					tag : {
+						maxLength : 150,
+						required : true
+					},
+					slug : {
+						maxLength : 150,
+						required: false
+					},
+					description : {
+						maxLength : 150,
+						required: false
+					},
+					category : {
+						maxLength: 150,
+						required : false
+					},
+					color : {
+						maxLength: 7,
+						required : false
+					}
+				};
+
+				return rules;
+			},
 		});
 
 		return TagModel;
