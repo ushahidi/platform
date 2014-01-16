309dd7e80c79b08d64f1e77f2a00ccf29838e391
Robbie Mackay <rm@robbiemackay.com>
2014-01-17 12:09:05 +1300

    Maintain 'selected' state on the item view

--- modules/UshahidiUI/media/js/app/views/PostListItemView.js
+++ modules/UshahidiUI/media/js/app/views/PostListItemView.js
@@ -7,8 +7,8 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-define(['handlebars', 'views/PostItemView', 'text!templates/PostListItem.html'],
-	function(Handlebars, PostItemView, template)
+define(['handlebars', 'underscore', 'views/PostItemView', 'text!templates/PostListItem.html'],
+	function(Handlebars, _, PostItemView, template)
 	{
 		//ItemView provides some default rendering logic
 		return PostItemView.extend(
@@ -16,6 +16,41 @@ define(['handlebars', 'views/PostItemView', 'text!templates/PostListItem.html'],
 			//Template HTML string
 			template: Handlebars.compile(template),
 			tagName: 'li',
-			className: 'list-view-post'
+			className: 'list-view-post',
+			initialize : function ()
+			{
+				this.selected = false;
+			},
+			events : {
+				'change .js-select-post-input' : 'updatedSelected',
+			},
+			select : function ()
+			{
+				this.selected = true;
+				this.$('.js-select-post-input').prop('checked', true);
+				this.trigger('select');
+			},
+			unselect : function ()
+			{
+				this.selected = false;
+				this.$('.js-select-post-input').prop('checked', false);
+				this.trigger('unselect');
+			},
+			updatedSelected : function (e)
+			{
+				var $el = this.$(e.currentTarget);
+				this.selected = $el.is(':checked');
+				this.trigger(this.selected ? 'select' : 'unselect');
+			},
+			serializeData: function()
+			{
+				var data = _.extend(
+					PostItemView.prototype.serializeData.call(this),
+					{
+						selected : this.selected
+					}
+				);
+				return data;
+			},
 		});
 	});
