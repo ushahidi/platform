b66d5013461da6f9a46c26718f980edf96cae5ec
Woody Gilk <woody@ushahidi.com>
2014-02-10 11:58:24 -0600

    Reorganize php.pp package list alphabetically

--- puppet/manifests/php.pp
+++ puppet/manifests/php.pp
@@ -1,13 +1,13 @@
 class base::php {
   $php_packages = [
-      "php5-mysqlnd",
-      "php5-mcrypt",
-      "php5-curl",
-      "php5-imap",
-      "php5-xcache",
-      "php5-gd",
-      "php5-xdebug",
       "php5-cli",
+      "php5-curl",
+      "php5-gd",
+      "php5-imap",
+      "php5-mcrypt",
+      "php5-mysqlnd",
+      "php5-xcache",
+      "php5-xdebug",
       "php-pear",
       "php5"
   ]
