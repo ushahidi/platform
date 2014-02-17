5e9396a8921ec6d06500a19075a8d7cb1db3f1aa
Henry Addo <henry@ushahidi.com>
2014-02-17 11:13:36 +0900

    Switch to using visually-hidden instead of hidden to hidel bulk action items for messages list

--- modules/UshahidiUI/media/js/app/views/MessageListView.js
+++ modules/UshahidiUI/media/js/app/views/MessageListView.js
@@ -59,13 +59,13 @@ define(['App', 'marionette', 'handlebars','underscore', 'views/MessageListItemVi
 
 				if ($checked.length > 0)
 				{
-					this.$('.js-list-view-bulk-actions').removeClass('hidden');
+					this.$('.js-list-view-bulk-actions').removeClass('visually-hidden');
 					this.$('.js-list-view-bulk-actions').addClass('visible');
 				}
 				else
 				{
 					this.$('.js-list-view-bulk-actions').removeClass('visible');
-					this.$('.js-list-view-bulk-actions').addClass('hidden');
+					this.$('.js-list-view-bulk-actions').addClass('visually-hidden');
 				}
 			},
 
