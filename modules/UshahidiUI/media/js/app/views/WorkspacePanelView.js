642e05d98ad4e8bc0107aef408546917da35ae4f
Woody Gilk <woody@ushahidi.com>
2014-03-28 15:11:09 -0500

    Explicitly declare underscore in workspace panel view, refs T76

--- modules/UshahidiUI/media/js/app/views/WorkspacePanelView.js
+++ modules/UshahidiUI/media/js/app/views/WorkspacePanelView.js
@@ -7,8 +7,8 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-define(['marionette', 'handlebars', 'App', 'text!templates/WorkspacePanel.html'],
-	function(Marionette, Handlebars, App, template)
+define(['underscore', 'marionette', 'handlebars', 'App', 'text!templates/WorkspacePanel.html'],
+	function(_, Marionette, Handlebars, App, template)
 	{
 		return Marionette.ItemView.extend(
 		{
