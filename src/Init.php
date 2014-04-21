c68670f00d4ee1f9e2ac0c84a8990eb5061db3e9
Woody Gilk <woody@ushahidi.com>
2014-04-21 17:41:41 -0500

    Use composer for autoloading, fixes T197
    Summary:
    - remove Pimple from `src/`, install with composer
    - remove custom autoloader, register with composer
    
    Test Plan:
    - run `composer install`
    - try app... if it works, nothing broke
    
    Reviewers: rjmackay
    
    Reviewed By: rjmackay
    
    Subscribers: dkobia
    
    Maniphest Tasks: T197
    
    Differential Revision: https://phabricator.ushahidi.com/D47

--- src/Init.php
+++ src/Init.php
@@ -11,38 +11,13 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-// register this directory as an include path
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(realpath(__FILE__)));
-
-// register a new autoloader for the Ushahidi namespace
-spl_autoload_register(function($class) {
-	$base_dir = dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR;
-	$ns = 'Ushahidi\\';
-	$len = strlen($ns);
-
-	// verify this is the expected namespace
-	if (strncmp($ns, $class, $len) !== 0) {
-		return;
-	}
-	
-	// replace:
-	// - namespace  -> base
-	// - backslash  -> directory separator
-	// append: .php
-	$file = $base_dir . str_replace('\\', '/', $class) . '.php';
-
-	if (file_exists($file)) {
-		require $file;
-	}
-});
+require __DIR__ . '/../vendor/autoload.php';
 
 // global depdendency container, using Pimple:
 // http://pimple.sensiolabs.org/
 function service($what = null) {
 	static $container;
 	if (!$container) {
-		require 'Pimple/Pimple.php';
-
 		$container = new \Pimple(array(
 			'config.storage' => '\Ushahidi\Storage\Config',
 			// 'user.storage' => '\Ushahidi\Storage\User\KohanaORM',
