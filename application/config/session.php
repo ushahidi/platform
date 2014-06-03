932f6ebd27eef12544962ebc78116a015d55e6c4
Robbie Mackay <rm@robbiemackay.com>
2014-06-04 08:28:55 +1200

    Don't gitignore application/config/ only the subdirs. Add default configs.
    Summary:
    Since we have environment configs, the gitignore for application/config/*.php makes less sense.
    We want to include default config, but exclude dev configs.
    
    Thoughts?
    
    Test Plan: None.
    
    Reviewers: eyedol, shadowhand
    
    Reviewed By: shadowhand
    
    Differential Revision: https://phabricator.ushahidi.com/D146

---
+++ application/config/session.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+/**
+ * Kohana Session Config
+ *
+ * @author     Ushahidi Team <team@ushahidi.com>
+ * @package    Ushahidi\Application\Config
+ * @copyright  2013 Ushahidi
+ * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
+ */
+
+return array(
+	'native' => array(
+		// @TODO change this to be project-specific
+		'name' => 'ushahidi',
+	),
+);
