5faecede08bac0b598af89abb15030a78fee8274
Henry Addo <henry@ushahidi.com>
2014-06-05 06:42:34 +0900

    Add a migration to replace the redirect uri

---
+++ application/migrations/oauth-league/20140604205210.php
@@ -0,0 +1,36 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Migration_Oauth_league_20140604205210 extends Minion_Migration_Base {
+
+	/**
+	 * Run queries needed to apply this migration
+	 *
+	 * @param Kohana_Database $db Database connection
+	 */
+	public function up(Kohana_Database $db)
+	{
+
+		$replace = URL::base(true, true) . 'user/oauth';
+
+		$query = DB::update('oauth_client_endpoints')
+			->set(array('redirect_uri' => DB::expr("REPLACE(`redirect_uri`,
+				'/user/oauth','$replace')")));
+
+		$query->execute($db);
+	}
+
+	/**
+	 * Run queries needed to remove this migration
+	 *
+	 * @param Kohana_Database $db Database connection
+	 */
+	public function down(Kohana_Database $db)
+	{
+		// Revert
+		$query = DB::udate('oauth_client_endpoints')
+			->set(array('redirect_uri'))
+			->values(array('/user/oauth'));
+		$query->execute($db);
+	}
+
+}
