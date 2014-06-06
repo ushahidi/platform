e549105040ca2c4b833875c127649b88cdff3657
Woody Gilk <woody@ushahidi.com>
2014-06-06 00:01:10 -0500

    Fix bad merge in SettingsView, refs T317

--- modules/UshahidiUI/media/js/app/settings/SettingsView.js
+++ modules/UshahidiUI/media/js/app/settings/SettingsView.js
@@ -7,7 +7,7 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-define([ 'App', 'modules/config', 'marionette', 'handlebars', 'underscore', 'text!templates/Settings.html'],
+define([ 'App', 'modules/config', 'marionette', 'handlebars', 'underscore', 'text!settings/Settings.html'],
 	function( App, config, Marionette, Handlebars, _, template)
 	{
 		return Marionette.ItemView.extend( {
