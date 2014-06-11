f6b58c71e912d9b5965e1fe547ea0e6ea88d629d
Henry Addo <henry@ushahidi.com>
2014-06-11 15:20:46 +0900

    Replace backbone model with backbone model factory
    Summary:
    Fix for #346.
    
    Replace `Backbone.Model({});` with `Backbone.ModelFactory({});`
    
    Test Plan:
     - Make sure API explorer is enabled
     - load #apiexplorer
     - Initiate any of the API endpoints
     - See the endpoint get executed
    
    Reviewers: shadowhand, rjmackay
    
    Reviewed By: rjmackay
    
    Maniphest Tasks: T346
    
    Differential Revision: https://phabricator.ushahidi.com/D172

--- modules/UshahidiUI/media/js/app/models/ApiExplorerModel.js
+++ modules/UshahidiUI/media/js/app/models/ApiExplorerModel.js
@@ -1,5 +1,14 @@
-define(['backbone'],
+/**
+ * ApiExplorer Model
+ *
+ * @module     ApiExplorerModel
+ * @author     Ushahidi Team <team@ushahidi.com>
+ * @copyright  2013 Ushahidi
+ * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
+ */
+
+define(['backbone', 'backbone-model-factory'],
 	function(Backbone)
 	{
-		return Backbone.Model({});
+		return Backbone.Model;
 	});
