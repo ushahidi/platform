6e553c24e41be449adc5c83148c5ebad35bfb056
Robbie Mackay <rm@robbiemackay.com>
2014-06-20 13:56:26 +1200

    remove backbone-model-factory dependency from ApiExplorerModel

--- modules/UshahidiUI/media/js/app/models/ApiExplorerModel.js
+++ modules/UshahidiUI/media/js/app/models/ApiExplorerModel.js
@@ -7,7 +7,7 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-define(['backbone', 'backbone-model-factory'],
+define(['backbone'],
 	function(Backbone)
 	{
 		return Backbone.Model;
