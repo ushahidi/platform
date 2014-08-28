b273e570659f3e5b56a4167a937aace5652f8676
Woody Gilk <woody@ushahidi.com>
2014-08-28 10:12:17 -0500

    actually just remove no-js entirely, refs 61065fcc32f6ac38139f0cc587554a55524a40e1

--- web/views/index.php
+++ web/views/index.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>
-<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<html class="lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="no-js" lang="en"> <!--<![endif]-->
+<html lang="en"> <!--<![endif]-->
 <head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -73,8 +73,5 @@
 	<span class="hide">Loading...</span>
 </div>
 <?php endif; ?>
-<script type="text/javascript">
-jQuery(function($) { $('html').removeClass('no-js').addClass('js'); });
-</script>
 </body>
 </html>
