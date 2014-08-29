d34a2561149af7bc6867827c3837e48426fee2ad
Robbie Mackay <rm@robbiemackay.com>
2014-08-29 21:50:22 +1200

    Fix JShint build errors after landing i18next. Refs T497

--- web/media/js/app/util/App.handlebars.js
+++ web/media/js/app/util/App.handlebars.js
@@ -110,18 +110,21 @@ define(['handlebars', 'underscore', 'moment', 'i18next', 'modules/config', 'unde
 		});
 
 		Handlebars.registerHelper('t', function(i18n_key, options) {
-			var opts = i18n.functions.extend(options.hash, this);
-			var result = i18n.t(i18n_key, opts);
+			var opts = i18n.functions.extend(options.hash, this),
+				result = i18n.t(i18n_key, opts);
+
 			return new Handlebars.SafeString(result);
 		});
 		Handlebars.registerHelper('tr', function(i18n_key, options) {
-			var opts = i18n.functions.extend(options.hash, this);
+			var opts = i18n.functions.extend(options.hash, this),
+				result;
+
 			if (options.fn)
 			{
 				opts.defaultValue = options.fn(this);
 			}
 
-			var result = i18n.t(i18n_key, opts);
+			result = i18n.t(i18n_key, opts);
 
 			return new Handlebars.SafeString(result);
 		});
