30f5b2596128cc279fc7aa9017d419a01c81c825
Robbie Mackay <rm@robbiemackay.com>
2014-09-04 09:39:31 +1200

    Load translation resources with absolute urls
    Summary: - Prefix i18n resource path with baseurl
    
    Test Plan: Go to http://ushv3.dev/posts/1 and see that translations load for menu titles
    
    Reviewers: bodacea, shadowhand
    
    Reviewed By: shadowhand
    
    Maniphest Tasks: T675
    
    Differential Revision: https://phabricator.ushahidi.com/D310

--- web/media/js/app/Main.js
+++ web/media/js/app/Main.js
@@ -18,7 +18,7 @@ require(['Config'], function () {
 		window.App = App;
 		i18n.init({
 				fallbackLng: 'en',
-				resGetPath: 'media/locales/__lng__/__ns__.json'
+				resGetPath: window.config.baseurl + 'media/locales/__lng__/__ns__.json'
 			}, function ()
 			{
 				App.appRouter = new AppRouter(
