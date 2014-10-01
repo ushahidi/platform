b5453504ef46f0175735cd1b65b774c22c07b109
Robbie Mackay <rm@robbiemackay.com>
2014-10-01 17:32:48 +1300

    Fix jshint error from a375632

--- web/media/js/app/routers/AppRouter.js
+++ web/media/js/app/routers/AppRouter.js
@@ -7,8 +7,8 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-define(['marionette', 'App'],
-	function(Marionette, App) {
+define(['marionette'],
+	function(Marionette) {
 		var router = Marionette.AppRouter.extend(
 		{
 			appRoutes :
