5e498d142d72057dcd3078d5a5a813b5a2ebb605
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-11-03 00:37:59 -0500

    Unified Test Runner
    Summary: this adds a single command that can be used to run all tests and receive a single result, both in the VM and in TravisCI
    
    Test Plan:
    run `bin/tests` to install the prereqs and run all the tests
    
    run `bin/tests help` to see the command usage
    
    run `bin/tests install` to install the prereqs then `bin/tests run --no-install` to run without installing (which is what TravisCI will do using `before_script` & `script`)
    
    try running from any current working directory... the script should work from anywhere
    
    I tried using https://github.com/jolicode/JoliCi to test the travis script, but couldn't get it to run properly. Both travis commands work when tested locally, and the exit codes are correct, so the travis build should work but will need to be tested live.
    
    Reviewers: rjmackay, vladimir, shadowhand
    
    Reviewed By: shadowhand
    
    Differential Revision: https://phabricator.ushahidi.com/D459

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -33,7 +33,7 @@ package {
 
 # MySQL setup
 class { '::mysql::server':
-	root_password    => 'root',
+	root_password    => '',
 	package_name     => 'mysql-server-5.5',
 	override_options => {
 		'mysqld' => {
