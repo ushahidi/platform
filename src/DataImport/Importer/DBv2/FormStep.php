2ea66e9c7f6a757251c169957950114df1f3b7de
Robbie Mackay <rm@robbiemackay.com>
2015-04-07 19:13:06 +1200

    Reorg form step too

--- src/DataImport/Importer/DBv2/FormStep.php
+++ src/DataImport/Importer/DBv2/FormStep.php
@@ -26,31 +26,42 @@ class FormStep implements ImportStep
 	use WriterTrait, ResourceMapTrait;
 
 	/**
+	 * Get post reader
+	 * @return Ddeboer\DataImport\Reader
+	 */
+	protected function getReader()
+	{
+		return new Reader\PdoReader($options['connection'], 'SELECT * FROM form ORDER BY id ASC');
+	}
+
+	/**
+	 * Item transform callback
+	 * @param  Array  $item
+	 * @return Array
+	 */
+	public function transform($item)
+	{
+		return [
+			'original_id' => $item['id'],
+			'name' => $item['form_title'],
+			'description' => $item['form_description'],
+			'disabled' => $item['form_active'] ? 0 : 1,
+		];
+	}
+
+	/**
 	 * Run a data import step
 	 *
 	 * @return mixed
 	 */
 	public function run(Array $options)
 	{
-		$converter = new MappingItemConverter();
-		$converter->addMapping('id', 'original_id')
-			->addMapping('form_title', 'name')
-			->addMapping('form_description', 'description')
-			->addMapping('form_active', 'disabled')
-			;
-
-		$activeConverter = new CallbackValueConverter(function ($active) {
-			return $active ? 0 : 1;
-		});
-
 		$this->writer->setOriginalIdentifier('original_id');
 
-		$reader = new Reader\PdoReader($options['connection'], 'SELECT * FROM form ORDER BY id ASC');
-		$workflow = new Workflow($reader, $options['logger'], 'dbv2-forms');
+		$workflow = new Workflow($this->getReader(), $options['logger'], 'dbv2-incidents');
 		$result = $workflow
-			->addWriter($this->writer)
-			->addItemConverter($converter)
-			->addValueConverter('disabled', $activeConverter)
+			->addWriter($this->getWriter())
+			->addItemConverter(new CallbackItemConverter([$this, 'transform']))
 			->setSkipItemOnFailure(true)
 			->process()
 		;
