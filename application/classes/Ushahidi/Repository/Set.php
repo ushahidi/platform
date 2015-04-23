513573e1c68b38b4925ee8ad9c3b9b551d61d955
Robbie Mackay <rm@robbiemackay.com>
2015-04-23 13:00:37 +1200

    Allow searching sets by search / featured. Also sort by featured T1438

--- application/classes/Ushahidi/Repository/Set.php
+++ application/classes/Ushahidi/Repository/Set.php
@@ -44,18 +44,56 @@ class Ushahidi_Repository_Set extends Ushahidi_Repository
 		));
 		$record['created'] = time();
 		return $this->executeInsert($this->removeNullValues($record));
-		
+
 	}
 
 	// UpdateRepository
 	public function update(Entity $entity) {
-		return parent::update($entity->setState(['updated' => time()]));		
+		return parent::update($entity->setState(['updated' => time()]));
 	}
 
 	// SearchRepository
 	public function getSearchFields()
 	{
-		return ['user_id', 'q' /* LIKE name */];
+		return [
+			'user_id',
+			'q', /* LIKE name */
+			'search',
+			'featured',
+		];
+	}
+
+	// SearchRepository
+	public function setSearchParams(SearchData $search)
+	{
+		// Overriding so we can alter sorting logic
+		// @todo make it easier to override just sorting
+
+		$this->search_query = $this->selectQuery();
+
+		$sorting = $search->getSorting();
+
+		// Always return featured sets first
+		// @todo make this optional
+		$this->search_query->order_by('sets.featured', 'DESC');
+
+		if (!empty($sorting['orderby'])) {
+			$this->search_query->order_by(
+				$this->getTable() . '.' . $sorting['orderby'],
+				Arr::get($sorting, 'order')
+			);
+		}
+
+		if (!empty($sorting['offset'])) {
+			$this->search_query->offset($sorting['offset']);
+		}
+
+		if (!empty($sorting['limit'])) {
+			$this->search_query->limit($sorting['limit']);
+		}
+
+		// apply the unique conditions of the search
+		$this->setSearchConditions($search);
 	}
 
 	// Ushahidi_Repository
@@ -66,6 +104,16 @@ class Ushahidi_Repository_Set extends Ushahidi_Repository
 		if ($search->q)
 		{
 			$sets_query->where('name', 'LIKE', "%{$search->q}%");
+		}
+
+		if ($search->search !== null)
+		{
+			$sets_query->where('search', '=', (int)$search->search);
+		}
+
+		if ($search->featured !== null)
+		{
+			$sets_query->where('featured', '=', (int)$search->featured);
 		}
 
 		if ($search->user_id)
