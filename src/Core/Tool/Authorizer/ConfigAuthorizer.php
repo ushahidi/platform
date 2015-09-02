a15888a92a2d23c33b8048206d368a6bf192df4a
Robbie Mackay <rm@robbiemackay.com>
2015-09-02 15:50:02 +1200

    Make sure **noone** can write to the feature config group

--- src/Core/Tool/Authorizer/ConfigAuthorizer.php
+++ src/Core/Tool/Authorizer/ConfigAuthorizer.php
@@ -38,16 +38,27 @@ class ConfigAuthorizer implements Authorizer
 	 */
 	protected $public_groups = ['features', 'map', 'site'];
 
+	/**
+	 * Public config groups
+	 * @var [string, ...]
+	 */
+	protected $writable_groups = ['map', 'site', 'dataprovider'];
+
 	/* Authorizer */
 	public function isAllowed(Entity $entity, $privilege)
 	{
 		// These checks are run within the `User` context.
 		$user = $this->getUser();
 
-		// Then we check if a user has the 'admin' role. If they do they're
-		// allowed access to everything (all entities and all privileges)
+		// Then we check if a user has the 'admin' role.
 		if ($this->isUserAdmin($user)) {
-			return true;
+			if (in_array($privilege, ['create', 'update']) && !$this->isConfigWritable($entity)) {
+				// If a config group is *not* writable, even an admin cannot edit it
+				return false;
+			} else {
+				// .. but an admin can do *anything* else
+				return true
+			}
 		}
 
 		// If a config group is public then *anyone* can view it.
@@ -72,6 +83,25 @@ class ConfigAuthorizer implements Authorizer
 		}
 
 		if (in_array($entity->getId(), $this->public_groups)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check if a config group is writable
+	 * @param  Entity  $entity
+	 * @return boolean
+	 */
+	protected function isConfigWriteable(Config $entity)
+	{
+		// Config that is unloaded is treated as writable.
+		if (!$entity->getId()) {
+			return true;
+		}
+
+		if (in_array($entity->getId(), $this->writable_groups)) {
 			return true;
 		}
 
