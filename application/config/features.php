5027daec69e9b8173fd1ddcf0b4df55650dbf2ed
Robbie Mackay <rm@robbiemackay.com>
2015-09-02 15:58:49 +1200

    Validate available providers when saving provider config

--- application/config/features.php
+++ application/config/features.php
@@ -9,21 +9,24 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-return array(
-	// determines which features are available in a deployment - in contrast
-	// to which features are enabled or not - i.e. a feature can only ever be
-	// enabled if it is also available, but may be available, but not enabled
+return [
+	// Determines which features are available in a deployment
 
-	// post data views
-	'post_view_map' => TRUE,
-	'post_view_list' => TRUE,
-	'post_view_chart' => FALSE,
-	'post_view_timeline' => FALSE,
-	// available data sources
-	'smssync' => FALSE,
-	'frontlinesms' => FALSE,
-	'email' => FALSE,
-	'twilio' => FALSE,
-	'nexmo' => FALSE,
-	'twitter' => FALSE,
-);
+	// Post views
+	'views' => [
+		'map' => TRUE,
+		'list' => TRUE,
+		'chart' => FALSE,
+		'timeline' => FALSE,
+	],
+
+	// Data sources
+	'data-providers' => [
+		'smssync' => TRUE,
+		'twitter' => TRUE,
+		'frontlinesms' => FALSE,
+		'email' => FALSE,
+		'twilio' => FALSE,
+		'nexmo' => FALSE,
+	],
+];
