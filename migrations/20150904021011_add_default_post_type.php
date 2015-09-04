815df809b3cf695ff20015e070fab250f157edf7
Robbie Mackay <rm@robbiemackay.com>
2015-09-04 14:44:59 +1200

    Only add default post type if we have none already

--- migrations/20150904021011_add_default_post_type.php
+++ migrations/20150904021011_add_default_post_type.php
@@ -13,30 +13,31 @@ class AddDefaultPostType extends AbstractMigration
         $connection = $this->getAdapter()->getConnection();
         $adapter = $this->getAdapter();
 
-        // Create default post type
-        // Note: Timestamp here *may* be in a different timezone, since
-        // we're using mysql time, not PHP time.
-        $this->execute(
-            "INSERT INTO forms (name, description, type, created)
-            VALUES
-                ( 'Basic Post', 'Post with a location', 'report', UNIX_TIMESTAMP(NOW()) )
-            "
-        );
+        $count = $this->fetchRow("SELECT COUNT(*) AS form_count FROM forms");
 
-        $connection->prepare(
-            "INSERT INTO form_stages (label, priority, required, form_id)
-            VALUES
-                ( 'Structure', 0, 1, :form_id )
-            "
-        )->execute([ ':form_id' => $connection->lastInsertId() ]);
+        // If we don't yet have any post types..
+        if ($count['form_count'] == 0) {
+            // .. create a default post type
+            // Note: Timestamp here *may* be in a different timezone, since
+            // we're using mysql time, not PHP time.
+            $this->execute(
+                "INSERT INTO forms (name, description, type, created)
+                VALUES ( 'Basic Post', 'Post with a location', 'report', UNIX_TIMESTAMP(NOW()) )"
+            );
 
-        $attrInsert = $connection->prepare(
-            "INSERT INTO form_attributes (" . $adapter->quoteColumnName('key') . ",
-                label, input, type, required, priority, cardinality, form_stage_id)
-            VALUES
-                ( 'location_default', 'Location', 'location', 'point', 0, 0, 1, :form_stage_id )
-            "
-        )->execute([ ':form_stage_id' => $connection->lastInsertId() ]);
+            $connection->prepare(
+                "INSERT INTO form_stages (label, priority, required, form_id)
+                VALUES ( 'Structure', 0, 1, :form_id )"
+            )->execute([ ':form_id' => $connection->lastInsertId() ]);
+
+            $attrInsert = $connection->prepare(
+                "INSERT INTO form_attributes (" . $adapter->quoteColumnName('key') . ",
+                    label, input, type, required, priority, cardinality, form_stage_id)
+                VALUES
+                    ( 'location_default', 'Location', 'location', 'point', 0, 0, 1, :form_stage_id )
+                "
+            )->execute([ ':form_stage_id' => $connection->lastInsertId() ]);
+        }
     }
 
     /**
