b4510dd5eabc7840a5b1bc014076658da626d3c6
Will <will.doran@riseup.net>
2015-10-21 12:32:23 -0400

    changed tag to category

--- application/messages/tag.php
+++ application/messages/tag.php
@@ -3,6 +3,6 @@
 return array(
 	'isSlugAvailable' => ':field :value is already in use',
   'description.regex' => 'The description must contain only letters, numbers, spaces and punctuation',
-  'tag.regex' => 'The tag name must contain only letters, numbers, spaces and punctuation',
+  'tag.regex' => 'The category name must contain only letters, numbers, spaces and punctuation',
 );
 
