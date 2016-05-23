59a7d1aa2b11f6682fbab54ff790cdf9ae85b314
David Losada <david@tuxpiper.com>
2016-05-23 18:48:16 +0200

    ushahidi/platform#1006 add build routine on docker to generate .tgz with dependencies

---
+++ docker/build.run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+check_vols_src() {
+  if [ ! -d /vols/src ]; then
+    echo "No /vols/src with code"
+    exit 1
+  fi
+}
+check_vols_out() {
+  if [ ! -d /vols/out ]; then
+    echo "No /vols/out for output!"
+    exit 1
+  fi
+}
+
+function sync {
+  check_vols_src
+  {
+    for f in bin/*; do
+      echo "- ${f}"
+    done
+    for f in modules/*; do
+      echo "- ${f}"
+    done
+    echo "- .git"
+    echo "- vendor"
+  } > /tmp/rsync_exclude
+  rsync -arv --exclude-from=/tmp/rsync_exclude --delete-during /vols/src/ ./
+}
+
+function run_composer_install {
+  composer install
+}
+
+function bundle {
+  check_vols_out
+  tar -cz -f /vols/out/platform-$(date -u +%Y%m%d-%H%M%S).tgz .
+}
+
+sync
+run_composer_install
+bundle
