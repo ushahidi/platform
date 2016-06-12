abd18843784d94eca4945c324536f4aa78a376d1
Robbie Mackay <rm@robbiemackay.com>
2016-06-12 15:27:36 +1200

    Update saved searches
    - Rename "Unstructured posts" to "Incoming messages"
    - Remove "All posts"

---
+++ migrations/20160612032112_update_default_saved_searches.php
@@ -0,0 +1,30 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class UpdateDefaultSavedSearches extends AbstractMigration
+{
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->execute("DELETE FROM sets WHERE name = 'All posts' AND filter = '{ \"status\": \"all\" }'");
+        $this->execute("UPDATE sets SET name = 'New messages', description = 'Incoming posts without no assigned survey' WHERE name = 'Unstructured posts'");
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->execute(
+            "INSERT INTO `sets` (`name`, `description`, `filter`, `view`, `visible_to`, `search`, `featured`)
+            VALUES
+                ('All posts', 'All posts', '{ \"status\": \"all\" }', 'list', '[]', 1, 1)
+            "
+        );
+        $this->execute("UPDATE sets SET name = 'Unstructured posts', description = 'Unstructured posts' WHERE name = 'New messages'");
+    }
+}
