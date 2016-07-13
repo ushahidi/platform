ab13154c6f0a62946842c6ad9b7bfe7840af4f10
Robbie Mackay <rm@robbiemackay.com>
2016-07-13 15:35:35 +1200

    Make sure mysql is up before running migrations

--- docker/test.run.sh
+++ docker/test.run.sh
@@ -39,8 +39,8 @@ function wait_for_mysql {
 sync
 run_composer_install
 cp .env.testing .env
+wait_for_mysql
 bin/phinx migrate -c application/phinx.php
 php -S localhost:8000 -t httpdocs httpdocs/index.php &
-wait_for_mysql
 
 exec $*
