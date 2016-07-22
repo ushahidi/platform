e834389e2443d0598e9be477540fbf22d814c09f
Robbie Mackay <rm@robbiemackay.com>
2016-07-22 20:18:47 +1200

    Add Raven Logger

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -129,6 +129,8 @@ if (getenv("RAVEN_URL"))
 	$error_handler->registerExceptionHandler();
 	$error_handler->registerErrorHandler();
 	$error_handler->registerShutdownFunction();
+
+	Kohana::$log->attach(new Log_Raven($client));
 }
 
 /**
