4495b5bfe9c825642e6e4e7a18d02d503a121016
Zack Halloran <zack@ushahidi.com>
2016-08-11 17:22:31 -0700

    Added `getRolesThatCanCreatePosts` function to the Form Repository
     - Added `getRolesThatCanCreatePosts` function to the Form Repository
    to allow for a single query look up of roles that have the right to add
    posts to the selected form
    - Updated PostAuthorizer to use `getRolesThatCanCreatePosts` function
    to check for access
    - Updated the PostAuthorizer to also check if the user has access to
    add a post to a form on Update.
    - Cleaned up PostAuthorizer & init.php to remove calls to FormRole repo
    and the Role repo

--- src/Init.php
+++ src/Init.php
@@ -383,8 +383,6 @@ $di->set('authorizer.post', $di->lazyNew('Ushahidi\Core\Tool\Authorizer\PostAuth
 $di->params['Ushahidi\Core\Tool\Authorizer\PostAuthorizer'] = [
 	'post_repo' => $di->lazyGet('repository.post'),
 	'form_repo' => $di->lazyGet('repository.form'),
-	'role_repo' => $di->lazyGet('repository.role'),
-	'form_role_repo' => $di->lazyGet('repository.form_role'),
 	];
 
 $di->set('authorizer.console', $di->lazyNew('Ushahidi\Console\Authorizer\ConsoleAuthorizer'));
