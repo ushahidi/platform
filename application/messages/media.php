80381f6a28b527b5f04d874f178f1e0328b55372
will <will.doran@riseup.net>
2016-08-20 11:08:46 -0400

    Changed file size error message to human readable, added error messages for mime type, added tests for image file size and mime fails

---
+++ application/messages/media.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return [
+    'range' => 'The file size should be less than :param3 KB',
+    'mime_type_not_allowed' => 'File type not supported. Please upload an image file.',
+    'mime_not_empty' => 'MIME type not found. The MIME type must be set for the file.'
+];
