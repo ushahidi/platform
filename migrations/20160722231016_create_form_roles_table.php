33a8d4f979370c07122454cbcf122da74ac820f2
Robbie Mackay <rm@robbiemackay.com>
2016-09-01 21:16:57 +1200

    Cascade changes to form or role id into form_roles

--- migrations/20160722231016_create_form_roles_table.php
+++ migrations/20160722231016_create_form_roles_table.php
@@ -9,8 +9,8 @@ class CreateFormRolesTable extends AbstractMigration
         $this->table('form_roles')
             ->addColumn('form_id', 'integer', ['null' => false])
             ->addColumn('role_id', 'integer', ['null' => false])
-            ->addForeignKey('form_id', 'forms', 'id', array('delete'=> 'CASCADE', 'update'=> 'NO_ACTION'))
-            ->addForeignKey('role_id', 'roles', 'id', array('delete'=> 'CASCADE', 'update'=> 'NO_ACTION'))
+            ->addForeignKey('form_id', 'forms', 'id', ['delete'=> 'CASCADE', 'update'=> 'CASCADE'])
+            ->addForeignKey('role_id', 'roles', 'id', ['delete'=> 'CASCADE', 'update'=> 'CASCADE'])
             ->create()
             ;
     }
