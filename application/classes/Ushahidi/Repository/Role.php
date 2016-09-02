e7266415ba2d746438397c4024eb929af2e6e1ce
Zack Halloran <zhalloran@users.noreply.github.com>
2016-09-01 19:50:56 -0700

    Feature/add review workflow toggle (#1318)
    * Add require_approval to Form entity
    * Add everyone_can_create to Form entity
    * Add form_roles table to record roles which can_create posts in a survey
    * Update post authorizer to honor who can create for each form
    * Update post validator to check status based on required approval settings
    * Add new tests
    * Architecture: Give validators access to full entity as well as changes.
      - Avoids nasty overrides in UpdatePost to pass needed info to validator
    * Add auto-publish for forms that don't require approval
      - Posts added to a form with `require_approval` set to false will be created with `status` = ‘published’.
    
    Refs #1282

--- application/classes/Ushahidi/Repository/Role.php
+++ application/classes/Ushahidi/Repository/Role.php
@@ -156,6 +156,13 @@ class Ushahidi_Repository_Role extends Ushahidi_Repository implements
 		if (!$role) { return false; }
 		return (bool) $this->selectCount(['name' => $role]);
 	}
+	
+	// Ushahidi_Repository
+	public function idExists($role_id = null)
+	{
+		if (!$role_id) { return false; }
+		return (bool) $this->selectCount(['id' => $role_id]);
+	}	
 
 	// RoleRepository
 	public function getByName($name)
