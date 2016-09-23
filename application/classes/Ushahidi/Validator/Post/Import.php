7e9554729792ceadedc662cfdd43fc13a8efbc9b
will <will.doran@riseup.net>
2016-09-23 13:22:48 -0400

    Changes based on review

--- application/classes/Ushahidi/Validator/Post/Import.php
+++ application/classes/Ushahidi/Validator/Post/Import.php
@@ -9,26 +9,13 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-use Ushahidi\Core\Entity;
-use Ushahidi\Core\Entity\FormAttributeRepository;
-use Ushahidi\Core\Entity\FormStageRepository;
-use Ushahidi\Core\Entity\UserRepository;
-use Ushahidi\Core\Entity\FormRepository;
-use Ushahidi\Core\Entity\PostRepository;
-use Ushahidi\Core\Entity\RoleRepository;
-use Ushahidi\Core\Entity\PostSearchData;
-use Ushahidi\Core\Tool\Validator;
-use Ushahidi\Core\Traits\UserContext;
-use Ushahidi\Core\Traits\PermissionAccess;
-use Ushahidi\Core\Traits\AdminAccess;
-use Ushahidi\Core\Traits\Permissions\ManagePosts;
-use Ushahidi\Core\Usecase\Post\UpdatePostRepository;
-use Ushahidi\Core\Usecase\Post\UpdatePostTagRepository;
 
 class Ushahidi_Validator_Post_Import extends Ushahidi_Validator_Post_Create
 {
   protected function getRules()
 	{
+    // We remove the rules validating required stages
+    // as stages are not validated during an import
 		return array_merge(parent::getRules(), [
       'values' => [
 				[[$this, 'checkValues'], [':validation', ':value', ':fulldata']]
