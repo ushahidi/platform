3a4bc45e7270ac94ba46b16b66ce1e81856e90f1
Robbie Mackay <rm@robbiemackay.com>
2016-10-18 16:29:56 +1300

    Format post_date output when exporting to CSV
    Fixes error converting date object to string

--- application/classes/Ushahidi/Formatter/Post/CSV.php
+++ application/classes/Ushahidi/Formatter/Post/CSV.php
@@ -58,6 +58,9 @@ class Ushahidi_Formatter_Post_CSV implements Formatter
 		{
 			unset($record['attributes']);
 
+			// Transform post_date to a string
+			$record['post_date'] = $record['post_date']->format("Y-m-d H:i:s");
+
 			foreach ($record as $key => $val)
 			{
 				// Assign form values
