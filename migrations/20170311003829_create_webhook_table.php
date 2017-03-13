7ffd7cc561ee7a63537b8556818001d7d3eeb637
will <will.doran@riseup.net>
2017-03-13 17:11:27 -0400

    Removing unintended commit of certain fles

--- migrations/20170311003829_create_webhook_table.php
+++ migrations/20170311003829_create_webhook_table.php
@@ -5,19 +5,6 @@ use Phinx\Migration\AbstractMigration;
 class CreateWebhookTable extends AbstractMigration
 {
     /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
-     *
-    public function change()
-    {
-    }
-    */
-
-    /**
      * Migrate Up.
      */
     public function up()
--- migrations/20170311004159_create_webhook_queue.php
+++ migrations/20170311004159_create_webhook_queue.php
@@ -4,18 +4,6 @@ use Phinx\Migration\AbstractMigration;
 
 class CreateWebhookQueue extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
-     *
-    public function change()
-    {
-    }
-    */
 
     /**
      * Migrate Up.
