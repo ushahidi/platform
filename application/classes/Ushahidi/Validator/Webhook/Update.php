fd784ba749df48c7848710fa721fdb0e14f45ca9
will <will.doran@riseup.net>
2017-03-16 21:52:32 -0400

    Fixing validator

--- application/classes/Ushahidi/Validator/Webhook/Update.php
+++ application/classes/Ushahidi/Validator/Webhook/Update.php
@@ -32,6 +32,7 @@ class Ushahidi_Validator_Webhook_Update extends Validator
 				['max_length', [':value', 255]],
 				// alphas, numbers, punctuation, and spaces
 				['regex', [':value', '/^[\pL\pN\pP ]++$/uD']],
+			],
 			'shared_secret' => [
 				['min_length', [':value', 20]],
 				// alphas, numbers, punctuation, and spaces
