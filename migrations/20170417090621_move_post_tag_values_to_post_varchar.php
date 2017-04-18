91e7e1789da37e63459f8a456098297fdb026102
Anna Iosif <hej@annaiosif.se>
2017-04-18 23:42:23 +0200

    adding migration to move post-tag-values to post_varchar #1397

--- migrations/20170417090621_move_post_tag_values_to_post_varchar.php
+++ migrations/20170417090621_move_post_tag_values_to_post_varchar.php
@@ -3,13 +3,48 @@
 use Phinx\Migration\AbstractMigration;
 
 class MovePostTagValuesToPostVarchar extends AbstractMigration
-{   
+{
     /**
      * Migrate Up.
      */
     public function up()
     {
-    
+        $pdo = $this->getAdapter()->getConnection();
+         // fetching posts with form_attribute_id
+        $posts = $this->fetchAll(
+            "SELECT posts.id, posts.form_id, form_attributes.id as form_attribute_id
+            FROM posts
+            INNER JOIN form_stages
+            ON form_stages.form_id = posts.form_id
+            INNER JOIN form_attributes
+              ON form_attributes.form_stage_id = form_stages.id"
+        );
+        $insert = $pdo->prepare(
+            "INSERT into
+                post_varchar
+                (`post_id`, `form_attribute_id`, `value`, `created`)
+                VALUES(:post_id, :form_attribute_id, :value, :created)"
+        );
+        foreach ($posts as $post) {
+                $post_tags = $pdo->prepare(
+                    "SELECT tag_id
+                    FROM posts_tags
+                    WHERE post_id = :post_id"
+                );
+                $post_tags->execute([':post_id' => $post['id']]);
+            // inserting post_ids and tag_ids(value) in post_varchar
+            $tags = $post_tags->fetchAll();
+            foreach ($tags as $tag) {
+                $insert->execute(
+                    [
+                    ':post_id' => $post['id'],
+                    ':form_attribute_id' => $post['form_attribute_id'],
+                    ':value' => $tag['tag_id'],
+                    ':created' => time()
+                    ]
+                );
+            }
+        }
     }
 
     /**
@@ -17,6 +52,38 @@ class MovePostTagValuesToPostVarchar extends AbstractMigration
      */
     public function down()
     {
+         $pdo = $this->getAdapter()->getConnection();
+        $posts = $this->fetchAll(
+            "SELECT posts.id, posts.form_id, form_attributes.id as form_attribute_id
+            FROM posts
+            INNER JOIN form_stages
+            ON form_stages.form_id = posts.form_id
+            INNER JOIN form_attributes
+              ON form_attributes.form_stage_id = form_stages.id"
+        );
+        $delete = $pdo->prepare(
+            "DELETE from post_varchar
+                WHERE post_id = :post_id AND form_attribute_id = :form_attribute_id AND value = :value"
+        );
 
+        foreach ($posts as $post) {
+            $post_tags = $pdo->prepare(
+                "SELECT tag_id
+                FROM posts_tags
+                WHERE post_id = :post_id"
+            );
+            $post_tags->execute([':post_id' => $post['id']]);
+            // inserting post_ids and tag_ids(value) in post_varchar
+            $tags = $post_tags->fetchAll();
+            foreach ($tags as $tag) {
+                $delete->execute(
+                    [
+                    ':post_id' => $post['id'],
+                    ':form_attribute_id' => $post['form_attribute_id'],
+                    ':value' => $tag['tag_id']
+                    ]
+                );
+            }
+        }
     }
 }
