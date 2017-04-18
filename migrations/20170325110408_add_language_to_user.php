ee12a729cb6aa3beacc2da94bbde8fc8afbe58fa
Anna Iosif <hej@annaiosif.se>
2017-04-18 23:49:37 +0200

    fixing linting and removing comment #1651

--- migrations/20170325110408_add_language_to_user.php
+++ migrations/20170325110408_add_language_to_user.php
@@ -3,20 +3,7 @@
 use Phinx\Migration\AbstractMigration;
 
 class AddLanguageToUser extends AbstractMigration
-{
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
-     *
-    public function change()
-    {
-    }
-    */
-    
+{   
     /**
      * Migrate Up.
      */
