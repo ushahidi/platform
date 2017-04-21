b524475f145fae2ac075c825e925edd13690f6f4
Robbie Mackay <rm@robbiemackay.com>
2017-04-21 16:36:51 +1200

    Direction httpdocs over to public/

--- httpdocs/coverage.php
+++ httpdocs/coverage.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $coverage = new \SebastianBergmann\CodeCoverage\CodeCoverage;
 $coverage->setAddUncoveredFilesFromWhitelist(true);
--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,22 +1,4 @@
 <?php
 
-// Initialize the Kohana application
-require __DIR__ . '/../application/kohana.php';
-
-/**
- * Some hosters set $_SERVER['ORIG_PATH_INFO'] instead of the more standard
- * $_SERVER['PATH_INFO'], we check for that here. Otherwise, let Kohana try to
- * automatically detect the URI.
- */
-$server_path = TRUE;
-if (array_key_exists('ORIG_PATH_INFO', $_SERVER)) {
-  $server_path = $_SERVER['ORIG_PATH_INFO'];
-}
-/**
- * Execute the main request. Use the URI passed in the first parameter, if not specified
- * (defaults to TRUE), we will try to automatically detect the URI.
- */
-echo Request::factory($server_path, array(), FALSE)
-	->execute()
-	->send_headers(TRUE)
-	->body();
+// Head on over to the standard lumen setup
+include __DIR__ . '/../public/index.php';
