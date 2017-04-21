032dc9fe9c602cc405ab41283107dbb712421daf
Robbie Mackay <rm@robbiemackay.com>
2017-04-21 16:51:05 +1200

    Clean up config controller

--- app/Http/Controllers/API/ConfigController.php
+++ app/Http/Controllers/API/ConfigController.php
@@ -16,27 +16,9 @@ use Ushahidi\App\Http\Controllers\RESTController;
 class ConfigController extends RESTController
 {
 
-    // protected $_action_map = array
-    // (
-    //     Http_Request::GET     => 'get',
-    //     Http_Request::PUT     => 'put', // Typically Update..
-    //     Http_Request::OPTIONS => 'options',
-    // );
-
     protected function getResource()
     {
         return 'config';
     }
-
-    // protected function _is_auth_required()
-    // {
-    //     if (parent::_is_auth_required())
-    //     {
-    //         // Completely anonymous access is allowed for (some) GET requests.
-    //         // Further checks are made down the stack.
-    //         return ($this->request->method() !== Request::GET);
-    //     }
-    //     return FALSE;
-    // }
 
 }
