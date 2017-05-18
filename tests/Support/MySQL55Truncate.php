6e7b4ca0bc18389c4c6ba2325b5dfaa9aac24064
Robbie Mackay <rm@robbiemackay.com>
2017-05-18 14:52:26 +1200

    Move MySQL5Truncate class into Tests\Support

--- tests/Support/MySQL55Truncate.php
+++ tests/Support/MySQL55Truncate.php
@@ -6,9 +6,11 @@
  * @package    DbUnit
  */
 
-class Unittest_Database_Operation_MySQL55Truncate extends PHPUnit_Extensions_Database_Operation_Truncate {
+namespace Tests\Support;
 
-	public function execute(PHPUnit_Extensions_Database_DB_IDatabaseConnection $connection, PHPUnit_Extensions_Database_DataSet_IDataSet $dataSet)
+class MySQL55Truncate extends \PHPUnit_Extensions_Database_Operation_Truncate {
+
+	public function execute(\PHPUnit_Extensions_Database_DB_IDatabaseConnection $connection, \PHPUnit_Extensions_Database_DataSet_IDataSet $dataSet)
 	{
 		$connection->getConnection()->query("SET @FAKE_PREV_foreign_key_checks = @@foreign_key_checks");
 		$connection->getConnection()->query("SET foreign_key_checks = 0");
