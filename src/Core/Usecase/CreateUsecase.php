a7a5fdcc690a2e74d2cf00bf8490d855c08a53a8
Carolyn Commons <crcommons@gmail.com>
2017-08-02 17:14:56 -0400

    removing whitespace

--- src/Core/Usecase/CreateUsecase.php
+++ src/Core/Usecase/CreateUsecase.php
@@ -64,15 +64,19 @@ class CreateUsecase implements Usecase
 	{
 		// Fetch a default entity and apply the payload...
 		$entity = $this->getEntity();
+
 		// ... verify that the entity can be created by the current user
 		$this->verifyCreateAuth($entity);
 
 		// ... verify that the entity is in a valid state
 		$this->verifyValid($entity);
+
 		// ... persist the new entity
 		$id = $this->repo->create($entity);
+
 		// ... get the newly created entity
 		$entity = $this->getCreatedEntity($id);
+
 		// ... check that the entity can be read by the current user
 		if ($this->auth->isAllowed($entity, 'read')) {
 			// ... and either return the formatted entity
@@ -99,7 +103,6 @@ class CreateUsecase implements Usecase
 	protected function getEntity()
 	{
 		return $this->repo->getEntity()->setState($this->payload);
-		
 	}
 
 	/**
