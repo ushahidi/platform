5dd06035b995c5e8715b6b79f587c68aa1b7625d
Mark <mark@kinstelli.com>
2017-10-16 21:54:37 -0500

    starting formatter methods to hydrate user info

--- application/classes/Ushahidi/Formatter/Post/Changelog.php
+++ application/classes/Ushahidi/Formatter/Post/Changelog.php
@@ -15,4 +15,43 @@ use Ushahidi\Core\Traits\FormatterAuthorizerMetadata;
 class Ushahidi_Formatter_Post_Changelog extends Ushahidi_Formatter_API
 {
 	use FormatterAuthorizerMetadata;
+
+	/**
+	 * Method that can add any kind of additional metadata about the entity,
+	 * by overloading this method in an extended class.
+	 *
+	 * Must return the formatted data!
+	 *
+	 * @param  Array  $data   formatted data
+	 * @param  Entity $entity resource
+	 * @return Array
+	 */
+	protected function add_metadata(Array $data, Entity $entity)
+	{
+		//extending the given data with hydrated values to be included in the GET changelog data
+		return $data;
+	}
+
+	protected function hydrateUserData()
+	{
+
+	}
+
+	/**
+	 * Format relations into url/id arrays
+	 * @param  string $resource resource name as used in urls
+	 * @param  int    $id       resource id
+	 * @return array
+	 */
+	protected function get_relation($resource, $id)
+	{
+		Kohana::$log->add(Log::INFO, 'Getting relation for id:'.print_r($id, true).' of resource: '.print_r($resource, true));
+
+		return !$id ? NULL : [
+			'id'  => intval($id),
+			'url' => URL::site(Ushahidi_Rest::url($resource, $id), Request::current()),
+		];
+	}
+
+
 }
