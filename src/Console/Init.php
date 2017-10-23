ba914d5e3c5fcef42282fbf3da28f689de0ac3fe
Robbie Mackay <rm@robbiemackay.com>
2017-10-23 16:05:24 +1300

    Remove old command and console application

--- src/Console/Init.php
+++ src/Console/Init.php
@@ -2,12 +2,6 @@
 
 $di = service();
 
-// Console application is used for command line tools.
-$di->set('app.console', $di->lazyNew('Ushahidi\Console\Application'));
-
-// Any command can be registered with the console app.
-$di->params['Ushahidi\Console\Application']['injectCommands'] = [];
-
 $di->set('authorizer.console', $di->lazyNew('Ushahidi\Console\Authorizer\ConsoleAuthorizer'));
     
     
