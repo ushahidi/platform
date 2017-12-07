7a1930813439fb66e7e0609b1cfb061a90702fe4
Robbie Mackay <rm@robbiemackay.com>
2017-12-07 21:45:03 +1300

    feat: Add tos flag to bin/ushahidi users create
    Adds tos flag to allow accepting terms at command line
    
    Refs #2026

--- src/Console/Command/User.php
+++ src/Console/Command/User.php
@@ -13,6 +13,7 @@ namespace Ushahidi\Console\Command;
 
 use Ushahidi\Console\Command;
 use Ushahidi\Core\Entity\UserRepository;
+use Ushahidi\Core\Entity\TosRepository;
 use Ushahidi\Core\Tool\Validator;
 use Ushahidi\Core\Exception\ValidatorException;
 use Ushahidi\Core\Exception\NotFoundException;
@@ -32,6 +33,13 @@ class User extends Command
 		$this->repo = $repo;
 	}
 
+	protected $tosRepo;
+
+	public function setTosRepo(TosRepository $tosRepo)
+	{
+		$this->tosRepo = $tosRepo;
+	}
+
 	public function setValidator(Validator $validator)
 	{
 		$this->validator = $validator;
@@ -48,6 +56,7 @@ class User extends Command
 			->addOption('role', ['r'], InputOption::VALUE_OPTIONAL, 'Role: admin, user')
 			->addOption('password', ['p'], InputOption::VALUE_REQUIRED, 'Password')
 			->addOption('with-hash', ['b'], InputOption::VALUE_OPTIONAL, 'password is already bcrypt hashed')
+			->addOption('tos', [], InputOption::VALUE_OPTIONAL, 'accept terms of service')
 			;
 	}
 
@@ -70,10 +79,11 @@ class User extends Command
 			'email' => $input->getOption('email'),
 			// Default to creating an admin user
 			'role' => $input->getOption('role') ?: 'admin',
-			'password' => $input->getOption('password'),
+			'password' => $input->getOption('password')
 		];
 
 		$passwordAlreadyHashed = $input->hasOption('with-hash') ?: 'false';
+		$acceptTos = $input->hasOption('tos');
 
 		if (!$this->validator->check($state)) {
 			throw new ValidatorException('Failed to validate user', $this->validator->errors());
@@ -82,6 +92,13 @@ class User extends Command
 		$entity = $this->repo->getEntity();
 		$entity->setState($state);
 		$id = $passwordAlreadyHashed ? $this->repo->createWithHash($entity) : $this->repo->create($entity);
+
+		$tos = $this->tosRepo->getEntity([
+			'user_id' => $id,
+			'tos_version_date' => getenv('TOS_RELEASE_DATE') ? date_create(getenv('TOS_RELEASE_DATE'), new \DateTimeZone('UTC')) : date_create()
+		]);
+
+		$this->tosRepo->create($tos);
 
 		return [
 			[
