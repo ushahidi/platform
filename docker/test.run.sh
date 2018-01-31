9f207d7be01a0d712f8677c5dad3a331c762bc9e
Robbie Mackay <rm@robbiemackay.com>
2018-01-31 14:50:26 +1300

    build: Make codeship run php dev server in subdirectory to avoid failures in PHP 7

--- docker/test.run.sh
+++ docker/test.run.sh
@@ -9,6 +9,6 @@ run_composer_install
 cp .env.testing .env
 wait_for_mysql
 composer pre-test
-php -S localhost:8000 -t httpdocs httpdocs/index.php &
+(cd httpdocs/; php -S localhost:8000 -t . index.php &)
 
 exec $*
