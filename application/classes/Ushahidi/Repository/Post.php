8aa65d44d2198f1880ca46e227dd9f29fc61a2e0
Romina Suarez <romina@ushahidi.com>
2018-02-20 14:58:27 -0300

    Remove unused getMediaForPost function

--- application/classes/Ushahidi/Repository/Post.php
+++ application/classes/Ushahidi/Repository/Post.php
@@ -135,7 +135,6 @@ class Ushahidi_Repository_Post extends Ushahidi_Repository implements
 			$data += [
 				'values' => $this->getPostValues($data['id']),
 				// Continued for legacy
-				// 'media' => $this->getMediaForPost($data['id']),
 				'tags'   => $this->getTagsForPost($data['id'], $data['form_id']),
 				'sets' => $this->getSetsForPost($data['id']),
 				'completed_stages' => $this->getCompletedStagesForPost($data['id']),
@@ -871,20 +870,6 @@ class Ushahidi_Repository_Post extends Ushahidi_Repository implements
 			->where('form_attribute_id', '=', $attr_id)
 			->execute($this->db);
 		return $result->as_array(NULL, 'tag_id');
-	}
-
-	/**
-	 * Get tags for a post
-	 * @param  int   $id  post id
-	 * @return array      tag ids for post
-	 */
-	private function getMediaForPost($id)
-	{
-		$result = DB::select('o_filename')->from('media')
-			->join('post_media')->on('value', '=', 'media.id')
-			->where('post_media.post_id', '=', $id)
-			->execute($this->db);
-		return $result->as_array(NULL, 'o_filename');
 	}
 
   /**
