d97b813a6e2826effda4d9f472520a9f8c18d085
Romina Suarez <romina@ushahidi.com>
2018-02-20 17:42:21 -0300

    Refactor to allow different postValues format for client vs csv

--- application/classes/Ushahidi/Repository/CSVPost.php
+++ application/classes/Ushahidi/Repository/CSVPost.php
@@ -9,34 +9,6 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
-use Ushahidi\Core\Entity;
-use Ushahidi\Core\Entity\FormRepository;
-use Ushahidi\Core\Entity\FormAttributeRepository;
-use Ushahidi\Core\Entity\FormStageRepository;
-use Ushahidi\Core\Entity\Permission;
-use Ushahidi\Core\Entity\Post;
-use Ushahidi\Core\Entity\PostLock;
-use Ushahidi\Core\Entity\PostLockRepository;
-use Ushahidi\Core\Entity\PostValueContainer;
-use Ushahidi\Core\Entity\PostRepository;
-use Ushahidi\Core\Entity\UserRepository;
-use Ushahidi\Core\SearchData;
-use Ushahidi\Core\Usecase\Post\StatsPostRepository;
-use Ushahidi\Core\Usecase\Post\UpdatePostRepository;
-use Ushahidi\Core\Usecase\Set\SetPostRepository;
-use Ushahidi\Core\Traits\UserContext;
-use Ushahidi\Core\Traits\Permissions\ManagePosts;
-use Ushahidi\Core\Tool\Permissions\AclTrait;
-use Ushahidi\Core\Traits\AdminAccess;
-use Ushahidi\Core\Tool\Permissions\Permissionable;
-use Ushahidi\Core\Traits\PostValueRestrictions;
-use Ushahidi\Core\Entity\ContactRepository;
-
-use Aura\DI\InstanceFactory;
-
-use League\Event\ListenerInterface;
-use Ushahidi\Core\Traits\Event;
-
 class Ushahidi_Repository_CSVPost extends Ushahidi_Repository_Post
 {
 	protected function getPostValues($id)
