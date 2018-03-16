565d3a451480afe3cf0bbbc57443f0ffc4701d0d
kinstelli <mark@ushahidi.com>
2018-03-16 11:27:16 -0500

    feat: table to assoc contacts with targeted_surveys and last question asked

---
+++ migrations/20180316161445_create_targeted_survey_state_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateTargetedSurveyStateTable extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->table('targeted_survey_state')
+            ->addColumn('form_id', 'integer', ['null' => false])
+            ->addColumn('contact_id', 'integer', ['null' => false])
+            ->addColumn('current_field_id', 'integer', ['null' => true])
+            ->addColumn('created', 'integer', ['default' => 0])
+            ->addColumn('updated', 'integer', ['default' => 0])
+            ->addForeignKey('form_id', 'forms', 'id', [
+                'delete' => 'CASCADE',
+                'update' => 'CASCADE',
+                ])
+            ->addForeignKey('contact_id', 'contacts', 'id', [
+                'delete' => 'CASCADE',
+                'update' => 'CASCADE',
+                ])
+            ->addForeignKey('current_field_id', 'form_attributes', 'id', [
+                'delete' => 'SET_NULL',
+                'update' => 'SET_NULL',
+                ])
+    		->create();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable('targeted_survey_state');
+    }
+}
