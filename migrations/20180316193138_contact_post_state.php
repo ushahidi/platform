4ac414a969590da1d6340cee4a0396c71778d455
Romina Suarez <romina@ushahidi.com>
2018-03-28 17:00:57 -0300

    Fix migrations

--- migrations/20180316193138_contact_post_state.php
+++ migrations/20180316193138_contact_post_state.php
@@ -2,28 +2,28 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class TargetedSurveyState extends AbstractMigration
+class ContactPostState extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class. targeted_survey_state table: post_id, status, contact_id
+	/**
+	 * Change Method.
+	 *
+	 * Write your reversible migrations using this method.
+	 *
+	 * More information on writing migrations is available here:
+	 * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+	 *
+	 * The following commands can be used in this method and Phinx will
+	 * automatically reverse them when rolling back:
+	 *
+	 *    createTable
+	 *    renameTable
+	 *    addColumn
+	 *    renameColumn
+	 *    addIndex
+	 *    addForeignKey
+	 *
+	 * Remember to call "create()" or "update()" and NOT "save()" when working
+	 * with the Table class. contact_post_state table: post_id, status, contact_id
 
 	 */
 
@@ -32,7 +32,7 @@ class TargetedSurveyState extends AbstractMigration
 	 */
 	public function up()
 	{
-		$this->table('targeted_survey_state')
+		$this->table('contact_post_state')
 			->addColumn('post_id', 'integer', ['null' => false])
 			->addColumn('contact_id', 'integer', ['null' => false])
 			->addColumn('status', 'string', ['null' => false, 'default' => 'PENDING'])
@@ -54,6 +54,6 @@ class TargetedSurveyState extends AbstractMigration
 	 */
 	public function down()
 	{
-		$this->dropTable('targeted_survey_state');
+		$this->dropTable('contact_post_state');
 	}
 }
--- migrations/20180327105646_contact_post_state_to_targeted_survey.php
+++ migrations/20180327105646_contact_post_state_to_targeted_survey.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class TargetedSurveyStateToTargetedSurvey extends AbstractMigration
+class ContactPostStateToTargetedSurvey extends AbstractMigration
 {
 
 	/**
