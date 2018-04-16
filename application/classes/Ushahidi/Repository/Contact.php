35e2a0aea8a3ef6c970c784f750e1d82de9bf86d
Romina Suarez <romina@ushahidi.com>
2018-04-16 17:45:07 -0300

    bugfix(Contacts) re#2753 - Make sure we account only for contacts in targted survey when in an ACTIVE survey

--- application/classes/Ushahidi/Repository/Contact.php
+++ application/classes/Ushahidi/Repository/Contact.php
@@ -133,8 +133,9 @@ class Ushahidi_Repository_Contact extends Ushahidi_Repository implements
         $query = DB::select('targeted_survey_state.contact_id', 'targeted_survey_state.form_id')
             ->from('targeted_survey_state')
             ->where('contact_id', '=', $contact_id)
-			->and_where('survey_status', '!=', Entity\TargetedSurveyState::SURVEY_FINISHED);
-
+			->and_where('survey_status', 'IN',
+				array(Entity\TargetedSurveyState::PENDING_RESPONSE, Entity\TargetedSurveyState::RECEIVED_RESPONSE)
+			);
         if($query->execute($this->db)->count() > 0)
         {
             Kohana::$log->add(Log::INFO, 'Contact is in a targeted survey: contact_id#'.print_r($contact_id, true));
