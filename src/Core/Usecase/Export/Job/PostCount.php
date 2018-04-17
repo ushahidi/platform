0404bd76e1390c8be95874fee2445850a3da527d
Romina Suarez <romina@ushahidi.com>
2018-04-17 16:24:29 -0300

    /External/xx endpoints need access to the user context that the exporter cli usess . This WIP allows that to happen by enabling some paths to not have [setUse]=session.user assigned

--- src/Core/Usecase/Export/Job/PostCount.php
+++ src/Core/Usecase/Export/Job/PostCount.php
@@ -12,12 +12,25 @@
 namespace Ushahidi\Core\Usecase\Export\Job;
 
 use Ushahidi\Core\Usecase\ReadUsecase;
-
+use Ushahidi\Core\Traits\UserContext;
 class PostCount extends ReadUsecase
 {
-    public function interact()
+
+	public function setUserRepo(\Ushahidi\Core\Entity\UserRepository $repo)
+	{
+		$this->userRepository = $repo;
+	}
+
+	public function interact()
 	{
         $entity = $this->getEntity();
+		/**
+		 * Enables user contexts when outside of an oauth request
+		 * for the user selected in the export job
+		 */
+		$userContextService = service('usercontext.service');
+        $user = $this->userRepository->get($entity->user_id);
+		$userContextService->setUser($user);
         return $this->repo->getPostCount($entity->id);
     }
 }
