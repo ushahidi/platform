d1c714a2620f7b9ef05e04be1ff43dd2c7c5c9ee
Robbie Mackay <rm@robbiemackay.com>
2018-04-19 16:57:50 +1200

    refactor: Make oauth scope migrations a noop in lumen

--- migrations/20180321121334_add_country_codes_to_scope.php
+++ migrations/20180321121334_add_country_codes_to_scope.php
@@ -10,7 +10,7 @@ class AddCountryCodesToScope extends AbstractMigration
      */
     public function up()
     {
-            $this->execute("INSERT INTO oauth_scopes (scope, name) VALUES ('country_codes', 'country_codes')");
+        // Noop after lumen migrations
     }
 
     /**
@@ -18,6 +18,6 @@ class AddCountryCodesToScope extends AbstractMigration
      */
     public function down()
     {
-            $this->execute("DELETE FROM oauth_scopes WHERE scope = 'country_codes'");
+        // Noop after lumen migrations
     }
 }
