0e91382d8c9a9898a9b93bee7ae2692ac3febbdc
Robbie Mackay <rm@robbiemackay.com>
2018-04-23 17:57:27 +1200

    fix: Fix AttributeRepo::getByForms failures when we have no form ids

--- src/App/Repository/Form/AttributeRepository.php
+++ src/App/Repository/Form/AttributeRepository.php
@@ -246,20 +246,23 @@ class AttributeRepository extends OhanzeeRepository implements
 	 */
 	public function getByForms($form_ids)
     {
-		$sql = "SELECT DISTINCT
-				form_attributes.*,
-				form_stages.priority as form_stage_priority,
-				form_stages.form_id as form_id
-			FROM form_attributes
-			INNER JOIN form_stages ON form_attributes.form_stage_id = form_stages.form_id
-			INNER JOIN forms ON form_stages.form_id = forms.id
-			where forms.id IN :forms
-			ORDER BY form_stages.priority, form_attributes.priority
-		";
-		$results = DB::query(Database::SELECT, $sql)
-			->bind(':forms', $form_ids)
-			->execute($this->db);
-		$attributes = $results->as_array();
+        $attributes = [];
+        if (count($form_ids) > 0) { // @FIXME: how would empty form_id even happen?
+			$sql = "SELECT DISTINCT
+					form_attributes.*,
+					form_stages.priority as form_stage_priority,
+					form_stages.form_id as form_id
+				FROM form_attributes
+				INNER JOIN form_stages ON form_attributes.form_stage_id = form_stages.form_id
+				INNER JOIN forms ON form_stages.form_id = forms.id
+				where forms.id IN :forms
+				ORDER BY form_stages.priority, form_attributes.priority
+			";
+			$results = DB::query(Database::SELECT, $sql)
+				->bind(':forms', $form_ids)
+				->execute($this->db);
+			$attributes = $results->as_array();
+		}
 
 		$native = [
 			[
