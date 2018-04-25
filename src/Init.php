6f58a25f5debbfabefa86f10e7cd3b81be36ba25
Robbie Mackay <rm@robbiemackay.com>
2018-04-26 08:37:53 +1200

    feat: Check parent authorization for tags
    Tags role was only verified for the tag itself, not its parent.
    Update to ensure we have access to a tags parent too

--- src/Init.php
+++ src/Init.php
@@ -438,6 +438,9 @@ $di->params['Ushahidi\Core\Tool\Authorizer\PostAuthorizer'] = [
 	'post_repo' => $di->lazyGet('repository.post'),
 	'form_repo' => $di->lazyGet('repository.form'),
 	];
+$di->params['Ushahidi\Core\Tool\Authorizer\TagAuthorizer'] = [
+	'tag_repo' => $di->lazyGet('repository.tag'),
+	];
 
 $di->set('authorizer.console', $di->lazyNew('Ushahidi\Console\Authorizer\ConsoleAuthorizer'));
 
