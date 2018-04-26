09d8523eaf03b7b635a7c0e3502df2ea1898d870
Robbie Mackay <rm@robbiemackay.com>
2018-04-26 20:39:32 +1200

    translations

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -63,7 +63,7 @@ ini_set('unserialize_callback_func', 'spl_autoload_call');
 /**
  * Set the default language
  */
-I18n::lang('en-us');
+I18n::lang('es-es');
 
 /**
  * Add response message for HTTP 422
