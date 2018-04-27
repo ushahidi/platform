04cd59dda5dc15cf92d9b08fb58f9d3a1749970d
Romina Suarez <romina@ushahidi.com>
2018-04-27 17:41:56 -0300

    Fix FormAttribute Repo to have Post status in the list of fields

--- application/classes/Ushahidi/Repository/Form/Attribute.php
+++ application/classes/Ushahidi/Repository/Form/Attribute.php
@@ -245,29 +245,37 @@ class Ushahidi_Repository_Form_Attribute extends Ushahidi_Repository implements
 		$sql = "SELECT DISTINCT form_attributes.*, form_stages.priority as form_stage_priority, form_stages.form_id as form_id " .
 			"FROM form_attributes " .
 			"INNER JOIN form_stages ON form_attributes.form_stage_id = form_stages.form_id " .
-			"INNER JOIN forms ON form_stages.form_id = forms.id " .
-			"where forms.id IN :forms ";
+			"INNER JOIN forms ON form_stages.form_id = forms.id ";
 		if (!empty($include_attributes))
 		{
 			$sql .= " AND form_attributes.id IN :form_attributes";
 		}
 		$sql .= "ORDER BY form_stages.priority, form_attributes.priority";
 		$results = DB::query(Database::SELECT, $sql)
-			->bind(':forms', $form_ids)
 			->bind(':form_attributes', $include_attributes)
 			->execute($this->db);
 		$attributes = $results->as_array();
 
 		$native = [
 			[
-			'label' => 'Post ID',
-			'key' => 'id',
-			'type' => 'integer',
-			'input' => 'number',
-			'form_id' => 0,
-			'form_stage_id' => 0,
-			'form_stage_priority' => 0,
-			'priority' => 1
+				'label' => 'Post ID',
+				'key' => 'id',
+				'type' => 'integer',
+				'input' => 'number',
+				'form_id' => 0,
+				'form_stage_id' => 0,
+				'form_stage_priority' => 0,
+				'priority' => 1
+			],
+			[
+				'label' => 'Post Status',
+				'key' => 'status',
+				'type' => 'string',
+				'input' => 'string',
+				'form_id' => 0,
+				'form_stage_id' => 0,
+				'form_stage_priority' => 0,
+				'priority' => 2
 			],
 			[
 				'label' => 'Created (UTC)',
@@ -277,7 +285,7 @@ class Ushahidi_Repository_Form_Attribute extends Ushahidi_Repository implements
 				'form_id' => 0,
 				'form_stage_id' => 0,
 				'form_stage_priority' => 0,
-				'priority' => 2
+				'priority' => 3
 			],
 			[
 				'label' => 'Updated (UTC)',
@@ -287,7 +295,7 @@ class Ushahidi_Repository_Form_Attribute extends Ushahidi_Repository implements
 				'form_id' => 0,
 				'form_stage_id' => 0,
 				'form_stage_priority' => 0,
-				'priority' => 3
+				'priority' => 4
 			],
 			[
 				'label' => 'Post Date (UTC)',
@@ -297,7 +305,7 @@ class Ushahidi_Repository_Form_Attribute extends Ushahidi_Repository implements
 				'form_id' => 0,
 				'form_stage_id' => 0,
 				'form_stage_priority' => 0,
-				'priority' => 4
+				'priority' => 5
 			],
 			[
 				'label' => 'Contact ID',
@@ -307,7 +315,7 @@ class Ushahidi_Repository_Form_Attribute extends Ushahidi_Repository implements
 				'form_id' => 0,
 				'form_stage_id' => 0,
 				'form_stage_priority' => 0,
-				'priority' => 5
+				'priority' => 6
 			],
 			[
 				'label' => 'Contact',
@@ -317,7 +325,7 @@ class Ushahidi_Repository_Form_Attribute extends Ushahidi_Repository implements
 				'form_id' => 0,
 				'form_stage_id' => 0,
 				'form_stage_priority' => 0,
-				'priority' => 6
+				'priority' => 7
 			],
 			[
 				'label' => 'Sets',
@@ -327,7 +335,7 @@ class Ushahidi_Repository_Form_Attribute extends Ushahidi_Repository implements
 				'form_id' => 0,
 				'form_stage_id' => 0,
 				'form_stage_priority' => 0,
-				'priority' => 7
+				'priority' => 8
 			]
 		];
 		return array_merge($native, $attributes);
