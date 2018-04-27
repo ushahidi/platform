6838a042b505ef0cd094603a8a187b9bd8a446ec
Anna <anna@ushahidi.com>
2018-04-27 12:19:40 +0200

    adding placeholder for spanish translations

--- application/i18n/es.php
+++ application/i18n/es.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
+// TODO: Add translations
 return array(
-	'Step 1: Download the "SMSSync" app from the Android Market.' => 'Spanish',
-	'Role must match the parent category' => 'El rol debe coincidir con la categoría padre'
+
 );
