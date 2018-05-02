af621f8734116dffcc8d7eacb55c637afc2d4fe3
Robbie Mackay <rm@robbiemackay.com>
2018-05-02 15:44:38 +1200

    feat: Replace built in migrate and seed commands with phinx

---
+++ app/Console/Commands/MigrateCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ushahidi\App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Phinx\Console\Command\Migrate as PhinxMigrateCommand;
+
+class MigrateCommand extends PhinxMigrateCommand
+{
+}
