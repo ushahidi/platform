cb0f10f11c89c58051843249a43494ae1d985482
vagrant <vagrant@platform-api>
2018-05-08 21:49:30 +0000

    linting

--- migrations/20180508154706_add_user_settings_table.php
+++ migrations/20180508154706_add_user_settings_table.php
@@ -7,31 +7,31 @@ class AddUserSettingsTable extends AbstractMigration
     /**
     * Migrate Up.
     */
-   public function up()
-   {
-       $this->table('user_settings')
-           ->addColumn('config_key', 'string', [
-               'null' => false,
-               'default' => false
-           ])
-           ->addColumn('config_value', 'string', [
-               'null' => false,
-               'default' => false
-           ])
-           ->addColumn('user_id', 'integer', ['null' => false])
-           ->addColumn('created', 'integer', ['default' => 0])
-           ->addColumn('updated', 'integer', ['default' => 0])
-           ->addForeignKey('user_id', 'users', 'id', [
-                    'delete' => 'CASCADE'
+    public function up()
+    {
+        $this->table('user_settings')
+            ->addColumn('config_key', 'string', [
+                'null' => false,
+                'default' => false
             ])
-           ->create();
-   }
+            ->addColumn('config_value', 'string', [
+                'null' => false,
+                'default' => false
+            ])
+            ->addColumn('user_id', 'integer', ['null' => false])
+            ->addColumn('created', 'integer', ['default' => 0])
+            ->addColumn('updated', 'integer', ['default' => 0])
+            ->addForeignKey('user_id', 'users', 'id', [
+               'delete' => 'CASCADE'
+            ])
+            ->create();
+    }
 
-   /**
+    /**
     * Migrate Down.
     */
-   public function down()
-   {
-       $this->dropTable('user_settings');
-   }
+    public function down()
+    {
+        $this->dropTable('user_settings');
+    }
 }
