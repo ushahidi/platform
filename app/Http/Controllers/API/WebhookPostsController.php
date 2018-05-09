2a10b91a88ebac9b149982357912ba63e36a1aae
Romina Suarez <romina@ushahidi.com>
2018-05-09 11:29:37 -0300

    fix: webhooks  was missing in the controller

--- app/Http/Controllers/API/WebhookPostsController.php
+++ app/Http/Controllers/API/WebhookPostsController.php
@@ -11,6 +11,7 @@ namespace Ushahidi\App\Http\Controllers\API;
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
+use Illuminate\Http\Request;
 use Ushahidi\App\Http\Controllers\RESTController;
 use Ushahidi\Core\Tool\Signer;
 
@@ -66,7 +67,7 @@ class WebhooksPosts extends RESTController
 	// 	}
 	// }
 
-	public function store()
+	public function store(Request $request)
 	{
         $this->usecase = $this->usecaseFactory
             ->get($this->getResource(), 'webhook-update')
