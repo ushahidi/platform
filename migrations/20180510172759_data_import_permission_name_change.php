eaf04de192b78003ba6ed0ac0aa4e72fa8fd2c68
Romina Suarez <romina@ushahidi.com>
2018-05-11 13:23:00 -0300

    refactor: change migration for license permission

--- migrations/20180510172759_data_import_permission_name_change.php
+++ migrations/20180510172759_data_import_permission_name_change.php
@@ -7,6 +7,11 @@ class DataImportPermissionNameChange extends AbstractMigration
 
 	public function up()
 	{
+
+        $this->execute("UPDATE 	permissions SET 
+							name = 'Bulk Data Import and Export', 
+							description = 'Import and export data in bulk'
+							WHERE name = 'Bulk Data Import';");
         $this->execute("UPDATE 	roles_permissions SET 
 							`permission` = 'Bulk Data Import and Export'
 							WHERE `permission` = 'Bulk Data Import';");
@@ -19,23 +24,19 @@ class DataImportPermissionNameChange extends AbstractMigration
         $this->execute("UPDATE 	roles_permissions SET 
 							`permission` = 'Bulk Data Import and Export'
 							WHERE `permission` = 'Bulk Data Import';");
-
-        $this->execute("UPDATE 	permissions SET 
-							name = 'Bulk Data Import and Export', 
-							description = 'Import and export data in bulk'
-							WHERE name = 'Bulk Data Import';");
 
 	}
 
 	public function down()
 	{
-		$this->execute("UPDATE 	roles_permissions SET 
-							permission = 'Bulk Data Import' 
-							WHERE permission = 'Bulk Data Import and Export';");
+
         $this->execute("UPDATE 	permissions SET 
 							name = 'Bulk Data Import', 
 							description = 'Import data from external sources'
 							WHERE name = 'Bulk Data Import and Export';");
+        $this->execute("UPDATE 	roles_permissions SET 
+							permission = 'Bulk Data Import' 
+							WHERE permission = 'Bulk Data Import and Export';");
 
     }
 }
