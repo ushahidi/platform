eb5319ac29a6c290065655669ab18c708cf116df
Romina Suarez <romina@ushahidi.com>
2018-05-14 18:58:38 -0300

    fix: namespace in licenserepository

--- src/App/Repository/HXL/HXLLicenseRepository.php
+++ src/App/Repository/HXL/HXLLicenseRepository.php
@@ -9,6 +9,7 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
  */
 
+namespace Ushahidi\App\Repository\HXL;
 
 use Ushahidi\Core\SearchData;
 use Ushahidi\Core\Entity\HXL\HXLLicense;
