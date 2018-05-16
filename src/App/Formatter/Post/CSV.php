9621df15fc361dcf124dca5537c62a8fef0addb8
Romina Suarez <rowasc@gmail.com>
2018-05-16 15:27:51 -0300

    refactor: divide into a smaller function for hdx row generation

--- src/App/Formatter/Post/CSV.php
+++ src/App/Formatter/Post/CSV.php
@@ -60,24 +60,25 @@ class CSV extends API
 
     public function generateHXLRows($header_rows, $hxl_rows) {
         foreach ($header_rows as $key => $label){
-            $tag = '';
-            //FIXME cleanup/divide this is 100% too complex
-            //FIXME handle multicolumn & lat/lon
-            foreach ($hxl_rows as $hxl_row) {
-                $key_set = explode('.', $key); //contains key + index of the key
-                $heading_key = isset($key_set[0]) ? $key_set[0] : null; // the heading type (sets, contact, title)
-                if ($hxl_row['key'] === $heading_key) {
-                    $tag = '#'.$hxl_row['tag_name'];
-                    $attribute = $hxl_row['attribute'] && !empty($hxl_row['attribute']) ?
-                                    ' +'. $hxl_row['attribute'] : '';
-                    $tag = $tag.$attribute;
-                }
-            }
-            $hxl[] = $tag;
+            $hxl[] = $this->generateHxlTagAndAttribute($key, $hxl_rows);
         }
         return $hxl;
     }
-
+    private function generateHxlTagAndAttribute($key, $hxl_rows) {
+        $tag = '';
+        //FIXME handle multi column values when CSV starts supporting them again
+        foreach ($hxl_rows as $hxl_row) {
+            $key_set = explode('.', $key); //contains key + index of the key
+            $heading_key = isset($key_set[0]) ? $key_set[0] : null; // the heading type (sets, contact, title)
+            if ($hxl_row['key'] === $heading_key) {
+                $tag = '#'.$hxl_row['tag_name'];
+                $attribute = $hxl_row['attribute'] && !empty($hxl_row['attribute']) ?
+                    ' +'. $hxl_row['attribute'] : '';
+                $tag = $tag.$attribute;
+            }
+        }
+        return $tag;
+    }
     public function setAddHeader($add_header)
     {
         $this->add_header = $add_header;
