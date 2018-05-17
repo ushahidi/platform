9e4989fcd7bae7c3c09343c1c2a3273aa9b4ffbf
will <will.doran@riseup.net>
2018-05-17 16:38:17 -0400

    Updatig exporter

--- routes/web.php
+++ routes/web.php
@@ -504,16 +504,23 @@ $router->group([
 
     // Web hooks
     $router->group([
-        'middleware' => ['auth:api', 'scope:webhooks'],
         'prefix' => 'webhooks'
     ], function () use ($router) {
-        $router->get('/', 'WebhooksController@index');
-        $router->post('/', 'WebhooksController@store');
-        $router->get('/{id:[0-9]+}', 'WebhooksController@show');
-        $router->put('/{id:[0-9]+}', 'WebhooksController@update');
-        $router->delete('/{id:[0-9]+}', 'WebhooksController@destroy');
-
-        $router->put('/posts', 'WebhookPostsController@update');
+        $router->group([
+            'middleware' => ['auth:api', 'scope:webhooks']
+        ], function () use ($router) {
+            $router->get('/', 'WebhooksController@index');
+            $router->post('/', 'WebhooksController@store');
+            $router->get('/{id:[0-9]+}', 'WebhooksController@show');
+            $router->put('/{id:[0-9]+}', 'WebhooksController@update');
+            $router->delete('/{id:[0-9]+}', 'WebhooksController@destroy');
+        });
+        $router->group([
+            'middleware' => ['signature'],
+            'prefix' => 'posts'
+        ], function () use ($router) {
+            $router->put('/', 'WebhookPostsController@update');
+        });
     });
 
     // HXL
