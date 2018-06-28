4fb12cc29f9dc0ccc3e6798a0c35664132dbdf75
Robbie Mackay <rm@robbiemackay.com>
2018-06-29 10:53:02 +1200

    feat: Better sanitize data sent to sentry
    - Sanitize HTTP headers sent to sentry
    - Add authToken, api_key, and client_secret to sanitized values

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -127,7 +127,21 @@ if (getenv("RAVEN_URL"))
 	$client = (
 		new Raven_Client(getenv("RAVEN_URL"), [
 			'exclude' => ['HTTP_Exception_404'],
-			'error_types' => (E_ALL & ~E_NOTICE & ~E_USER_NOTICE)
+			'error_types' => (E_ALL & ~E_NOTICE & ~E_USER_NOTICE),
+			'processors' => [
+				'Raven_Processor_SanitizeHttpHeadersProcessor',
+				'Raven_Processor_SanitizeDataProcessor'
+			],
+			'processorOptions' => [
+				'Raven_Processor_SanitizeDataProcessor' => [
+					'fields_re' => '/(authorization|password|passwd|secret|password_confirmation|card_number|auth_pw|authToken|api_key|client_secret)/i',
+				],
+				'Raven_Processor_SanitizeHttpHeadersProcessor' => [
+					'sanitize_http_headers' => [
+						'Authorization', 'Proxy-Authorization', 'X-Csrf-Token', 'X-CSRFToken', 'X-XSRF-TOKEN', 'X-Ushahidi-Signature',
+					]
+				]
+			]
 		])
 	)->install();
 
