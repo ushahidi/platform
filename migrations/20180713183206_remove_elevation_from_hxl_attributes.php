a4fd81bd953ee4bbceeb0cec48768c92953aaf48
Romina Suarez <rowasc@gmail.com>
2018-07-13 17:45:30 -0300

    fix(HXLAttributes Migration): remove elevation as an option for hxl tag #geo - fix style

--- migrations/20180713183206_remove_elevation_from_hxl_attributes.php
+++ migrations/20180713183206_remove_elevation_from_hxl_attributes.php
@@ -5,17 +5,16 @@ use Phinx\Migration\AbstractMigration;
 class RemoveElevationFromHxlAttributes extends AbstractMigration
 {
     /**
-     * Remove the 'elevation' HXL attribute.
+     * Remove the 'elevation' HXL attribute reference.
      * This attribute will be added back if we decide to implement elevation correctly
      * in platform + csv
      */
     public function change()
     {
         $this->execute(
-            "DELETE FROM hxl_tag_attributes where attribute_id IN (
-                    select id from hxl_attributes where attribute = 'elevation' 
+            "DELETE FROM hxl_tag_attributes WHERE attribute_id IN (
+                    SELECT id from hxl_attributes WHERE attribute = 'elevation' 
                 );"
         );
-
     }
 }
