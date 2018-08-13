8ba06cae202b0e56a46049744586be902c96f1de
will <will.doran@riseup.net>
2018-08-13 14:10:26 -0400

    Linting

--- app/Http/Middleware/CheckDemoExpiration.php
+++ app/Http/Middleware/CheckDemoExpiration.php
@@ -19,19 +19,14 @@ class CheckDemoExpiration
     {
         $multisite = config('multisite.enabled');
         if ($multisite && !$request->isMethod('get')) {
-
             if (service('site.config')) {
-
                 if (service('site.config')['tier'] === 'demo') {
-
-                    $expiration_date = strtotime( service('site.config')['expiration_date']);
+                    $expiration_date = strtotime(service('site.config')['expiration_date']);
                     $extension_date = strtotime(service('site.config')['extension_date']);
                     $now = new DateTime();
 
                     if ($expiration_date < $now) {
-
                         if (!$extension_date || $extension_date < $now) {
-                            
                             $expirationMessage = 'The demo period for this deployment has expired.';
 
                             abort(
