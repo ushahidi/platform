c8ac7aaadd28b6c89d5089f8e2c5c55b05d09c47
Anna <anna@ushahidi.com>
2018-08-14 15:41:05 +0200

    adding migration to change input type for title-field in forms

---
+++ migrations/20180814131012_changing_title_input_to_text.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ChangingTitleInputToText extends AbstractMigration
+{
+    public function up()
+    {
+        $pdo = $this->getAdapter()->getConnection();
+
+        $update = $pdo->prepare(
+            "UPDATE form_attributes SET input = 'text' WHERE type = 'title'
+                AND input = 'varchar'"
+        );
+
+        $update->execute();
+    }
+
+}
