34c40ebf1865f5495baeaab509be4bda25bcb8c7
will <will.doran@riseup.net>
2018-08-23 16:38:48 -0400

    fixing tests

--- app/Http/Middleware/CheckDemoExpiration.php
+++ app/Http/Middleware/CheckDemoExpiration.php
@@ -19,12 +19,13 @@ class CheckDemoExpiration
     {
         $multisite = config('multisite.enabled');
         $isDemoTier = service('site.config')['tier'] === 'demo';
-        $now = new DateTime();
-        $expiration_date = strtotime(service('site.config')['expiration_date']);
-        $extension_date = strtotime(service('site.config')['extension_date']);
         $isNotGet = !$request->isMethod('get');
 
         if ($multisite && $isNotGet && $isDemoTier) {
+            $now = new DateTime();
+            $expiration_date = strtotime(service('site.config')['expiration_date']);
+            $extension_date = strtotime(service('site.config')['extension_date']);
+
             if ($expiration_date < $now && (!$extension_date || $extension_date < $now)) {
                 abort(503, 'The demo period for this deployment has expired.');
             }
