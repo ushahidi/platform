d9f8fc5126706227c3bf9db3fc48d9937704cac1
Robbie Mackay <rm@robbiemackay.com>
2018-08-29 09:57:55 +1200

    fix(mail): Configure services (and stub other service config)

--- config/services.php
+++ config/services.php
@@ -19,4 +19,20 @@ return [
         'secret' => env('MAILGUN_SECRET'),
     ],
 
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
 ];
