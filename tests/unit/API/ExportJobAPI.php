8f010d0065b562ef827ff9279f0a9980b3048be7
Robbie Mackay <rm@robbiemackay.com>
2018-10-19 21:19:07 +1300

    test: Add integration test which run full CreateJob including queues

--- tests/unit/API/ExportJobAPI.php
+++ tests/unit/API/ExportJobAPI.php
@@ -13,12 +13,45 @@ use Faker;
 class ExportJobAPI extends TestCase
 {
     /**
-     * Get count
+     * Create a job
      */
     public function testCreateJob()
     {
         $this->withoutMiddleware();
         $this->expectsJobs(\Ushahidi\App\Jobs\ExportPostsJob::class);
+
+        $this
+            ->actingAs(new \Ushahidi\App\Auth\GenericUser(['id' => 2]))
+            ->json('POST', '/api/v3/exports/jobs', [
+            'fields' => 'test',
+            'filters' => [
+              'status' => ['published','draft']
+            ],
+            'entity_type' => 'post',
+            'send_to_browser' =>  true,
+            'send_to_hdx' =>  false,
+            'hxl_heading_row' => ['something']
+            ]);
+
+        $this->seeStatusCode('200')
+            ->seeJson([
+                'fields' => ['test'],
+                'filters' => [
+                  'status' => ['published','draft']
+                ],
+                'entity_type' => 'post',
+                'send_to_browser' =>  true,
+                'send_to_hdx' =>  false,
+                'hxl_heading_row' => null
+            ]);
+    }
+
+    /**
+     * Create a job
+     */
+    public function testCreateJobWithRealDispatch()
+    {
+        $this->withoutMiddleware();
 
         $this
             ->actingAs(new \Ushahidi\App\Auth\GenericUser(['id' => 2]))
