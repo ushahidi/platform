6625b0c74eae1116337cc965ce7b90f0bf0a8e14
Robbie Mackay <rm@robbiemackay.com>
2018-10-23 21:16:14 +1300

    refactor: Remove defunct SendExport console command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,6 @@ class Kernel extends ConsoleKernel
         \Ushahidi\Console\Command\UserDelete::class,
         \Ushahidi\Console\Command\Notification::class,
         \Ushahidi\Console\Command\PostExporter::class,
-        \Ushahidi\Console\Command\SendExport::class,
         \Ushahidi\Console\Command\SavedSearch::class,
         \Ushahidi\Console\Command\Webhook::class,
         \Ushahidi\Console\Command\ObfuscateData::class,
