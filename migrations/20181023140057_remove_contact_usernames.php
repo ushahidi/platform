3dfd5059e4ca7cfa3dd563ef48e82b85060094b7
Romina Suarez <rowasc@gmail.com>
2018-10-24 14:09:56 -0300

    Remove useless comments

--- migrations/20181023140057_remove_contact_usernames.php
+++ migrations/20181023140057_remove_contact_usernames.php
@@ -4,27 +4,6 @@ use Phinx\Migration\AbstractMigration;
 
 class RemoveContactUsernames extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change()
     {
         $this->execute("DELETE FROM contacts where id IN (select contact_id from messages where data_source='twitter')");
