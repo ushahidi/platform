76d71be278c9b9702703ca3ac90fdea989e9b2b4
Robbie Mackay <rm@robbiemackay.com>
2018-11-06 13:18:17 +1300

    fix: fix TestMultisiteJob and add command to run it

--- app/Jobs/TestMultisiteJob.php
+++ app/Jobs/TestMultisiteJob.php
@@ -3,6 +3,7 @@
 namespace Ushahidi\App\Jobs;
 
 use Illuminate\Support\Facades\Log;
+use Ushahidi\App\Multisite\MultisiteAwareJob;
 
 class TestMultisiteJob extends Job
 {
@@ -27,11 +28,19 @@ class TestMultisiteJob extends Job
     public function handle(\Ushahidi\App\Multisite\MultisiteManager $multisite)
     {
         // Get deployment ID
-        var_dump($multisite->getSiteId());
+        Log::debug('Site', [$multisite->getSite()]);
 
-        //
         // Get config
+        Log::debug(
+            'Site config',
+            [app(\Ushahidi\Core\Entity\ConfigRepository::class)->get('site')->asArray()]
+        );
+
         // Get an ohanzee DB connection
         // Get an illuminate DB connection
+        Log::debug(
+            'Export batch',
+            [app(\Ushahidi\Core\Entity\ExportBatchRepository::class)->getByJobId(10)]
+        );
     }
 }
