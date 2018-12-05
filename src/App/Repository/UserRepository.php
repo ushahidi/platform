10f9bcac7b812ea1cc677bdac4a09547d7080dc8
Robbie Mackay <rm@robbiemackay.com>
2018-12-06 10:53:32 +1300

    feat: Don't run bulk insert when innodb_autoinc_lock_mode isn't safe

--- src/App/Repository/UserRepository.php
+++ src/App/Repository/UserRepository.php
@@ -14,6 +14,7 @@
 namespace Ushahidi\App\Repository;
 
 use Ohanzee\DB;
+use Ohanzee\Database;
 use Ushahidi\Core\Entity;
 use Ushahidi\Core\Entity\User;
 use Ushahidi\Core\Entity\UserRepository as UserRepositoryContract;
@@ -112,6 +113,13 @@ class UserRepository extends OhanzeeRepository implements
     public function createMany(Collection $collection) : array
     {
         // Check MySQL `innodb_autoinc_lock_mode` = 0 or 1 before running
+        $lockMode = DB::query(Database::SELECT, "SHOW VARIABLES LIKE 'innodb_autoinc_lock_mode'")
+            ->execute($this->db())
+            ->get('Value');
+
+        if (!in_array((int) $lockMode, [0, 1])) {
+            throw new \RuntimeException('Cannot bulk insert users with innodb_autoinc_lock_mode = ' . $lockMode);
+        }
 
         $first = $this->removeNullValues($collection->first()->asArray());
         unset($first['contacts']);
