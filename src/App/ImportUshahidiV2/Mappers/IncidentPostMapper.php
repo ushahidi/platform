02e18b529f0c5803fa20ba3f637f5b1a883fb131
Robbie Mackay <rm@robbiemackay.com>
2018-12-07 17:10:55 +1300

    feat: Map location and verified fields onto form attributes

--- src/App/ImportUshahidiV2/Mappers/IncidentPostMapper.php
+++ src/App/ImportUshahidiV2/Mappers/IncidentPostMapper.php
@@ -4,16 +4,19 @@ namespace Ushahidi\App\ImportUshahidiV2\Mappers;
 
 use Ushahidi\Core\Entity;
 use Ushahidi\Core\Entity\Post;
+use Ushahidi\Core\Entity\FormAttributeRepository;
 use Ushahidi\App\ImportUshahidiV2\Contracts\Mapper;
 use Ushahidi\App\ImportUshahidiV2\Contracts\ImportMappingRepository;
 
 class IncidentPostMapper implements Mapper
 {
     protected $mappingRepo;
+    protected $attrRepo;
 
-    public function __construct(ImportMappingRepository $mappingRepo)
+    public function __construct(ImportMappingRepository $mappingRepo, FormAttributeRepository $attrRepo)
     {
         $this->mappingRepo = $mappingRepo;
+        $this->attrRepo = $attrRepo;
     }
 
     public function __invoke(array $input) : Entity
@@ -26,22 +29,33 @@ class IncidentPostMapper implements Mapper
             'status' => $input['incident_active'] ? 'published' : 'draft',
             'author_email' => $input['person_email'],
             'author_realname' => $input['person_first'] . ' ' . $input['person_last'],
-            // 'values' => [
-            //     $attributeMap['original_id'] => [$item['id']],
-            //     $attributeMap['date'] => [$item['incident_date']],
-            //     $attributeMap['location_name'] => [$item['location_name']],
-            //     $attributeMap['location'] => [[
-            //         'lat' => $item['latitude'],
-            //         'lon' => $item['longitude']
-            //     ]],
-            //     $attributeMap['verified'] => [$item['incident_verified']],
-            //     // news
-            //     // source
-            // ]
-
+            'post_date' => $input['incident_date'],
+            'values' => [
+                // @todo handle missing attributes!?
+                $this->getAttributeKey($input['form_id'], 'location_name')
+                    => [$input['location_name']],
+                $this->getAttributeKey($input['form_id'], 'location')
+                    => [['lat' => $input['latitude'], 'lon' => $input['longitude']]],
+                $this->getAttributeKey($input['form_id'], 'verified')
+                    => [$input['incident_verified']],
+                // news_source_link
+                // video_link
+                // photos
+                // categories
+            ]
         ]);
 
         // NB: We don't map some data ie:
         // - Custom form fields
+    }
+
+    public function getAttributeKey($formId, $column)
+    {
+        // Get attribute map <formid>-<attribute>
+        $id = $this->mappingRepo->getDestId('incident_column', $formId.'-'.$column);
+        // Load the actual attribute
+        $attribute = $this->attrRepo->get($id);
+        // Return the key
+        return $attribute->key ?? $column;
     }
 }
