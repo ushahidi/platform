a7f62c3462479b4507418e6af479065f0411b521
Robbie Mackay <rm@robbiemackay.com>
2018-12-10 13:59:55 +1300

    feat: Cache import mappings

--- src/App/ImportUshahidiV2/Repositories/ImportMappingRepository.php
+++ src/App/ImportUshahidiV2/Repositories/ImportMappingRepository.php
@@ -14,9 +14,15 @@ namespace Ushahidi\App\ImportUshahidiV2\Repositories;
 use Ushahidi\App\ImportUshahidiV2\ImportMapping;
 use Ushahidi\App\ImportUshahidiV2\Contracts\ImportMappingRepository as ImportMappingRepositoryContract;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class ImportMappingRepository /*extends EloquentRepository*/ implements ImportMappingRepositoryContract
 {
+    /**
+     * Cache lifetime in minutes
+     * @var integer
+     */
+    protected $cache_lifetime = 10;
 
     public function create(ImportMapping $model) : int
     {
@@ -38,10 +44,18 @@ class ImportMappingRepository /*extends EloquentRepository*/ implements ImportMa
 
     public function getDestId(int $importId, string $sourceType, $sourceId)
     {
-        return ImportMapping::where([
-            'import_id' => $importId,
-            'source_type' => $sourceType,
-            'source_id' => $sourceId,
-        ])->value('dest_id');
+        $key = "mapping.$importId.$sourceType.$sourceId";
+
+        return Cache::tags(['import_mapping'])->remember(
+            $key,
+            $this->cache_lifetime,
+            function () use ($importId, $sourceType, $sourceId) {
+                return ImportMapping::where([
+                    'import_id' => $importId,
+                    'source_type' => $sourceType,
+                    'source_id' => $sourceId,
+                ])->value('dest_id');
+            }
+        );
     }
 }
