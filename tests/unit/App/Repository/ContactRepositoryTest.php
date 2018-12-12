8ac8a6df0cb281d826a1869dbc1013909d12113d
Robbie Mackay <rm@robbiemackay.com>
2018-12-13 11:14:02 +1300

    test: Remove commented out ContactRepo tests

--- tests/unit/App/Repository/ContactRepositoryTest.php
+++ tests/unit/App/Repository/ContactRepositoryTest.php
@@ -20,51 +20,7 @@ use Database_MySQLi;
  */
 class ContactRepositoryTest extends \PHPUnit\Framework\TestCase
 {
-
-    protected $repository;
-
-    public function setUp()
+    public function testCreateMany()
     {
-        parent::setUp();
-        /* @TODO:
-            Use a fixture / mock to setup some testable data.
-
-        $dbconfig = [
-            'connection' => ['username' => 'homestead',
-            'hostname' => 'xxxxxxx',
-            'username' => 'xxxxxxx',
-            'database' => 'xxxxxxx',
-            'password' => 'xxxxxx',
-            'socket'   => '',
-            'port'     => 3306]];
-        $local_conn = '?';
-
-        $db = new Database_MySQLi($local_conn, $dbconfig);
-        $this->contactRepo = new Ushahidi_Repository_Contact($db); // not a mock!
-
-        @TODO: add known targeted contact data
-            e.g., $this->known_targeted_contact_phone = '2222222222';
-
-        @TODO: add known contact data thats not in a survey
-            e.g., $this->known_regular_contact_phone = '1111111111';
-
-        */
-    }
-
-
-    public function testIsInTargetedSurvey()
-    {
-        /*
-        $mock_payload = ['from'=> $this->known_targeted_contact_phone,
-                        'contact_type' => 'sms'];
-        $contact_mock = $this->contactRepo->getByContact($mock_payload['from'], $mock_payload['contact_type']);
-        $this->assertEquals($this->contactRepo->isInTargetedSurvey($contact_mock->getId()), true );
-
-        $mock_payload = ['from'=> $this->known_regular_contact_phone,
-                        'contact_type' => 'sms'];
-
-        $contact_mock = $this->contactRepo->getByContact($mock_payload['from'], $mock_payload['contact_type']);
-        $this->assertEquals($this->contactRepo->isInTargetedSurvey($contact_mock->getId()), false );
-        */
     }
 }
