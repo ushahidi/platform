b4b151c0e9462b84b64bdd4b9870ae04d05b1d3b
Arianna Lanz <ariannalanz@gmail.com>
2019-11-15 15:33:30 -0500

    build(CheckForInvalidJSON): build middleware to check for invalid JSON in the request body

---
+++ app/Http/Middleware/CheckForInvalidJSON.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ushahidi\App\Http\Middleware;
+
+use Closure;
+
+class CheckForInvalidJSON
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $data = json_decode($request->getContent());
+
+        // Check for NULL not empty - since [] and {} will be empty but valid
+        if ($data === null) {
+            // Get further error info
+            switch (json_last_error()) {
+                case JSON_ERROR_NONE:
+                    $error = 'No errors';
+                    break;
+                case JSON_ERROR_DEPTH:
+                    $error = 'Maximum stack depth exceeded';
+                    break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    $error = 'Underflow or the modes mismatch';
+                    break;
+                case JSON_ERROR_CTRL_CHAR:
+                    $error = 'Unexpected control character found';
+                    break;
+                case JSON_ERROR_SYNTAX:
+                    $error = 'Syntax error, malformed JSON';
+                    break;
+                case JSON_ERROR_UTF8:
+                    $error = 'Malformed UTF-8 characters, possibly incorrectly encoded';
+                    break;
+                default:
+                    $error = 'Unknown error';
+                    break;
+                }
+            }
+        
+        if (json_last_error() !== JSON_ERROR_NONE) {
+                abort(422, $error);
+            }
+
+        return $next($request);
+    }
+}
