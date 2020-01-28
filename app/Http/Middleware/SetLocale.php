6f4a801bf54be20daf2d8d7dc446afb58bbe8ac9
rowasc <rowasc@gmail.com>
2020-01-28 01:00:04 -0300

    feat: Add wilduran/negotiation to validate language headers - add ability to bypass header setup

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -17,27 +17,30 @@ class SetLocale
     public function handle($request, Closure $next, ...$features)
     {
         $langHeader = $request->header('Accept-language');
-        $negotiator = new LanguageNegotiator();
-        $priorities = config('language.locales');
-
-        /**
-         * NOTE: the negotiation library does not support providing a list of ordered elements in its
-         * stable version so defaulting to english is the best we can do right now without a more complex
-         * custom class. I'd rather we support the willduran/negotiation library if we go down that path.
-         */
-        $type = $fallback = $this->getPriorityFallback($langHeader, $priorities, 'en');
-        /**
-         * TODO:
-         * decide if we would like to work on extending willduran/negotiation and bring
-         * v3.0.3 to stable so that we have getOrderedElements available if it's still
-         * being maintained (or I guess fork if it is no longer mantained)
-        */
-        $bestLanguage = $negotiator->getBest($langHeader, $priorities);
-        if ($bestLanguage) {
-            $type = $bestLanguage->getType();
+        if ($langHeader) {
+            $negotiator = new LanguageNegotiator();
+            $priorities = config('language.locales');
+    
+            /**
+             * NOTE: the negotiation library does not support providing a list of ordered elements in its
+             * stable version so defaulting to english is the best we can do right now without a more complex
+             * custom class. I'd rather we support the willduran/negotiation library if we go down that path.
+             */
+            $type = $fallback = $this->getPriorityFallback($langHeader, $priorities, 'en');
+            /**
+             * TODO:
+             * decide if we would like to work on extending willduran/negotiation and bring
+             * v3.0.3 to stable so that we have getOrderedElements available if it's still
+             * being maintained (or I guess fork if it is no longer mantained)
+            */
+            $bestLanguage = $negotiator->getBest($langHeader, $priorities);
+            if ($bestLanguage) {
+                $type = $bestLanguage->getType();
+            }
+            app('translator')->setLocale($type);
+            app('translator')->setFallback($fallback);
         }
-        app('translator')->setLocale($type);
-        app('translator')->setFallback($fallback);
+        
         return $next($request);
     }
     /**
