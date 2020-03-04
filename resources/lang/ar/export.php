d33741d07925f14b9d35163dadfab1014a9bb871
David Losada <david@tuxpiper.com>
2020-03-04 21:48:43 +0100

    chore(translations): add transifex CLI tool config and contributed translations

---
+++ resources/lang/ar/export.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'sendToHDXShouldBeTrue' => 'يجب أن يكون send_to_hdx مفعّلاً عندما يكون send_to_browser غير مفعّل',
+    'sendToBrowserShouldBeTrue' => 'يجب أن يكون send_to_browser مفعّلاً عندما يكون send_to_hdx غير مفعّل',
+    'includeHXLShouldBeTrue' => 'يجب أن يكون include_hxl مفعّلاً عندما يكون send_to_hdx  مفعّلاً',
+);
---
+++ resources/lang/ar/layers.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'dataUrlOrMediaRequired' => 'يتوجب إضافة أحد data_url أو media_id',
+    'dataUrlMediaConflict' => 'يمكن تحديد واحد فقط من data_url أو media_id ',
+);
