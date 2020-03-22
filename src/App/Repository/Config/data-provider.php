e968a30fa1ba7e22bd17a1207b0a2b6df469131c
noone0212 <ssoni@ee.iitr.ac.in>
2020-03-22 05:39:59 +0530

    fix: Outgoingemail option removed

--- src/App/Repository/Config/data-provider.php
+++ src/App/Repository/Config/data-provider.php
@@ -12,13 +12,13 @@
 return [
     'providers' => [
         // List of data providers key=provider value=enabled
-        'smssync' => false,
-        'email' => false,
+        'smssync' => true,
+        'email' => true,
         'outgoingemail' => false,
-        'twilio' => false,
+        'twilio' => true,
         'nexmo' => false,
-        'twitter' => false,
-        'frontlinesms' => false
+        'twitter' => true,
+        'frontlinesms' => true
     ],
 
     // Config params for individual providers
