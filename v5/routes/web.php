0979bac99c85b76e84c00fdbff7e44194ab6bf52
Romina <rowasc@gmail.com>
2020-07-13 10:50:57 -0300

    Revert "Trying something"
    This reverts commit ed87edd9c8c1d9bc003688d6e281d606646c113c.

--- v5/routes/web.php
+++ v5/routes/web.php
@@ -8,16 +8,6 @@ $apiBase = 'api/v' . $apiVersion;
 $router->group([
     'prefix' => $apiBase,
 ], function () use ($router) {
-    $router->group([
-	'prefix' => 'ohnoes'
-    ],
-	function () use ($router) {
-		$router->get('/', function () {
-		return 'OHNO DID YOU BREAK THE API';
-
-		});
-	}
-    );
     // Forms
     $router->group([
         // 'namespace' => 'Forms',
