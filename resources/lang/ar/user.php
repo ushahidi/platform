2cff19711b06b32a6dccfbf585e5f4fbf3f26de5
Solymanx7 <solymanx7@gmail.com>
2020-08-12 21:24:54 +0200

    Update better translation

--- resources/lang/ar/user.php
+++ resources/lang/ar/user.php
@@ -3,7 +3,7 @@
 return array(
     'isUniqueEmail'    => 'الحقل :field مُستخدم بالفعل ',
     'isUniqueUsername' => 'الحقل :field مُستخدم بالفعل ',
-    'isUserSelf'       => 'لا يمكنك تغيير دورك بنفسك',
+    'isUserSelf'       => 'لا يمكنك تغيير صلاحياتك بنفسك',
     'adminUserLimitReached' => 'limit::admin',
     'invalidResetToken' => 'مفتاح إعادة ضبط غير صالح أو منتهي الصلاحية'
 );
