ad43d95e2fd8bb1325876511b0f76b6fc733b319
Solymanx7 <solymanx7@gmail.com>
2020-08-12 21:50:52 +0200

    Update media.php

--- resources/lang/ar/media.php
+++ resources/lang/ar/media.php
@@ -2,6 +2,6 @@
 
 return array(
     'size_error' => 'يجب أن يكون حجم الملف أقل من :param1 ميغابايت',
-    'mime_type_not_allowed' => 'نوع الملف غير مدعوم. يرجى تحميل ملف صورة.',
+    'mime_type_not_allowed' => 'نوع الملف غير مدعوم. يرجى تحميل صورة.',
     'mime_not_empty' => 'نوع MIME غير مدعوم. يجب اختيار نوع MIME لهذا الملف.'
 );
