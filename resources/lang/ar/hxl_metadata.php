066e398b114b90dd62e8910b946735b24917db82
Solymanx7 <solymanx7@gmail.com>
2020-08-12 21:32:43 +0200

    Update hxl_metadata.php

--- resources/lang/ar/hxl_metadata.php
+++ resources/lang/ar/hxl_metadata.php
@@ -2,6 +2,6 @@
 
 return array(
     'uniqueMetadataByJob' => 'لا يمكن إنشاء كيان بيانات وصفية لمهمة لديها كيان بالفعل',
-    'privateShouldNotBeEmpty' => 'يجب أن تكون قيمة الخاص هي بوليانية. وصلنا: NULL'
+    'privateShouldNotBeEmpty' => 'يجب أن تكون القيمة إما True or False. وصلنا: NULL'
 
 );
