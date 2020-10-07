443f608494b38807de3741a2398068f1e6f6232d
Petro-Ivvysoft <iqblackman@gmail.com>
2020-10-07 12:08:01 +0300

    Create layers.php

---
+++ resources/lang/ua/layers.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'dataUrlOrMediaRequired' => 'Потрібен один з data_url або media_id',
+    'dataUrlMediaConflict' => 'Можна визначити лише один з data_url або media_id',
+);
