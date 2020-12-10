609344beef5b5fea2f9c53e5d6b11abd21f3c1fe
Romina <rowasc@gmail.com>
2020-12-09 22:43:29 -0300

    fix(Messages): posts without an attached survey failed because we attempted to get ->survey->tasks for them

--- v5/Http/Resources/PostResource.php
+++ v5/Http/Resources/PostResource.php
@@ -33,6 +33,13 @@ class PostResource extends Resource
         // this is an unfortunate problem with using an old version of lumen
         // that doesn't let me do guest user checks without adding more risk.
         $privileges = $authorizer->getAllowedPrivs($entity);
+        $post_content =  Collection::make([]);
+
+        if ($no_values && $this->survey) {
+            $post_content = new TaskCollection($this->survey->tasks);
+        } elseif ($this->survey) {
+            $post_content = new PostValueCollection($col);
+        }
 
         return [
             'id' => $this->id,
@@ -53,15 +60,7 @@ class PostResource extends Resource
             'base_language' => $this->base_language,
             'categories' => $this->categories,
             'completed_stages' => $this->postStages,
-            'post_content' => function () use ($no_values, $col) {
-                if ($no_values && $this->survey) {
-                    return new TaskCollection($this->survey->tasks);
-                } elseif (!$this->survey) {
-                    return Collection::make([]);
-                } else {
-                    return new PostValueCollection($col);
-                }
-            },
+            'post_content' => $post_content,
             'translations' => new TranslationCollection($this->translations),
             'enabled_languages' => [
                 'default'=> $this->base_language,
