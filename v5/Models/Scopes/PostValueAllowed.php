90ee6d14b5c3be23c063fd553cb2ce58b38ebe2d
Wisdom Ebong <30173059+webong@users.noreply.github.com>
2022-04-04 14:51:48 +0100

    chore: modified v5 models

--- v5/Models/Scopes/PostValueAllowed.php
+++ v5/Models/Scopes/PostValueAllowed.php
@@ -15,7 +15,6 @@ namespace v5\Models\Scopes;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-use Ushahidi\Core\Entity\Permission;
 
 class PostValueAllowed implements Scope
 {
