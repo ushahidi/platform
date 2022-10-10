11e4a165738194378e0a3071c05ec4e17205deba
Wisdom Ebong <wisdom@ushahidi.com>
2022-10-10 09:30:15 +0100

    chore: Add a tool class that allows Laravel Resource adapt to Ushahidi Formatter

---
+++ src/Ushahidi/Core/Tool/ResourceFormatter.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Ushahidi Reader
+ *
+ * @author     Ushahidi Team <team@ushahidi.com>
+ * @package    Ushahidi\Application
+ * @copyright  2014 Ushahidi
+ * @license    https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
+ */
+
+namespace Ushahidi\Core\Tool;
+
+use Ushahidi\Contracts\Formatter;
+use Illuminate\Http\Resources\Json\Resource;
+
+class ResourceFormatter extends Resource implements Formatter
+{
+    public function __invoke($data)
+    {
+        return new self($data);
+    }
+}
