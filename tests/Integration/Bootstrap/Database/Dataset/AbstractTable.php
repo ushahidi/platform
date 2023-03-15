978c3fd3a951cc11f3678ced5b591d40a22515ab
Wisdom Ebong <30173059+webong@users.noreply.github.com>
2023-03-15 16:22:59 +0100

    chore: added doc blocks citing to the feature tests database code

--- tests/Integration/Bootstrap/Database/Dataset/AbstractTable.php
+++ tests/Integration/Bootstrap/Database/Dataset/AbstractTable.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file adapted from the DbUnit package.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ * (c) Ushahidi Team <team@ushahidi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Ushahidi\Tests\Integration\Bootstrap\Database\Dataset;
 
