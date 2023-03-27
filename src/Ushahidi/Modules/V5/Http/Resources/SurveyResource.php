323e5002829f8c30d4cc09d934337f82ce935897
mh-asmi <mnaasmi@gmail.com>
2023-03-27 14:18:40 +0300

    add allowed_privileges to survey and collections

--- src/Ushahidi/Modules/V5/Http/Resources/SurveyResource.php
+++ src/Ushahidi/Modules/V5/Http/Resources/SurveyResource.php
@@ -1,9 +1,27 @@
 <?php
 namespace Ushahidi\Modules\V5\Http\Resources;
 
+use Ushahidi\Core\Entity\Form as SurveyEntity;
+
 class SurveyResource extends BaseResource
 {
     public static $wrap = 'result';
+
+    private function getResourcePrivileges()
+    {
+        $authorizer = service('authorizer.form');
+        // Obtain v3 entity from the v5 post model
+        // Note that we use attributesToArray instead of toArray because the first
+        // would have the effect of causing unnecessary requests to the database
+        // (relations are not needed in this case by the authorizer)
+        $entity = new SurveyEntity($this->resource->attributesToArray());
+        // if there's no user the guards will kick them off already, but if there
+        // is one we need to check the authorizer to ensure we don't let
+        // users without admin perms create forms etc
+        // this is an unfortunate problem with using an old version of lumen
+        // that doesn't let me do guest user checks without adding more risk.
+        return $authorizer->getAllowedPrivs($entity);
+    }
 
     /**
      * Transform the resource into an array.
@@ -16,6 +34,8 @@ class SurveyResource extends BaseResource
         $data = json_decode($this->toJson(), true);
         $data['translations'] = (new TranslationCollection($this->translations))->toArray(null);
         $data['tasks'] = new TaskCollection($this->tasks);
+        $data['allowed_privileges']=  $this->getResourcePrivileges();
+
         return $data;
     }
 }
