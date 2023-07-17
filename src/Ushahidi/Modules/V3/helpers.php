03a7415ee85f0b50a9e68363e9a0bfdddcb8a58a
Wisdom Ebong <wisdom@ushahidi.com>
2023-07-17 08:04:46 +0100

    style: fix lint issues

--- src/Ushahidi/Modules/V3/helpers.php
+++ src/Ushahidi/Modules/V3/helpers.php
@@ -90,16 +90,23 @@ if (!function_exists('is_base64')) {
     /**
      * Convert a string to snake case.
      */
-    function is_base64($s){
+    function is_base64($s)
+    {
         // Check if there are valid base64 characters
-        if (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s)) return false;
+        if (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s)) {
+            return false;
+        }
 
         // Decode the string in strict mode and check the results
         $decoded = base64_decode($s, true);
-        if(false === $decoded) return false;
+        if (false === $decoded) {
+            return false;
+        }
 
         // Encode the string again
-        if(base64_encode($decoded) != $s) return false;
+        if (base64_encode($decoded) != $s) {
+            return false;
+        }
 
         return true;
     }
