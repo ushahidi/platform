6515e86a62a01392200ce7811150be33bd6bce0e
Mh-Asmi <mnaasmi@gmail.com>
2023-08-16 16:53:27 +0400

    fix update can-notify for contact

--- src/Ushahidi/Modules/V5/Http/Controllers/MediaController.php
+++ src/Ushahidi/Modules/V5/Http/Controllers/MediaController.php
@@ -4,9 +4,7 @@ namespace Ushahidi\Modules\V5\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Ushahidi\Modules\V5\Actions\Media\Queries\FetchMediaByIdQuery;
-use Ushahidi\Modules\V5\Actions\Media\Queries\FetchMediaQuery;
 use Ushahidi\Modules\V5\Http\Resources\Media\MediaResource;
-use Ushahidi\Modules\V5\Http\Resources\Media\MediaCollection;
 use Ushahidi\Modules\V5\Actions\Media\Commands\CreateMediaCommand;
 use Ushahidi\Modules\V5\Actions\Media\Commands\UpdateMediaCommand;
 use Ushahidi\Modules\V5\Actions\Media\Commands\DeleteMediaCommand;
@@ -28,24 +26,24 @@ class MediaController extends V5Controller
     public function show(int $id)
     {
         $media = $this->queryBus->handle(new FetchMediaByIdQuery($id));
-        $this->authorize('show', $media);
+        $this->authorizeAnyone('show', $media);
         return new MediaResource($media);
     } //end show()
 
 
 
-    /**
-     * Display the specified resource.
-     *
-     * @return MediaCollection
-     * @throws \Illuminate\Auth\Access\AuthorizationException
-     */
-    public function index(Request $request)
-    {
-        $this->authorize('index', Media::class);
-        $medias = $this->queryBus->handle(FetchMediaQuery::FromRequest($request));
-        return new MediaCollection($medias);
-    } //end index()
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @return MediaCollection
+    //  * @throws \Illuminate\Auth\Access\AuthorizationException
+    //  */
+    // public function index(Request $request)
+    // {
+    //     $this->authorizeAnyone('index', Media::class);
+    //     $medias = $this->queryBus->handle(FetchMediaQuery::FromRequest($request));
+    //     return new MediaCollection($medias);
+    // } //end index()
 
 
     /**
@@ -59,7 +57,7 @@ class MediaController extends V5Controller
     {
         $command = CreateMediaCommand::fromRequest($request);
         $new_media = new Media($command->getMediaEntity()->asArray());
-        $this->authorize('store', $new_media);
+        $this->authorizeAnyone('store', $new_media);
         return $this->show($this->commandBus->handle($command));
     } //end store()
 
