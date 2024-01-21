97ed69378c8c0f45abdee76e462b2cee2d85ee0b
David Losada <david@tuxpiper.com>
2024-01-21 20:06:41 +0100

    Bundle version github action (#4708)
    * chore(ci): create release file
    
    * chore(ci): create release file

---
+++ docker/release.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# if someone invokes this with bash
+set -e
+
+# build release tarball
+usage() {
+    echo usage: $0 VERSION
+    exit 2
+}
+
+test $# -eq 1 || usage
+VERSION=$1
+CWD=$(pwd)
+
+TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t platform-build)
+WORK_DIR=$TMP_DIR/ushahidi-platform-bundle-${VERSION}
+mkdir $WORK_DIR
+
+if [ -z "$DEST_DIR" ]; then
+    DEST_DIR="${CWD}/build"
+fi
+
+echo "Copy to temp dir"
+cp -R ./ $WORK_DIR
+
+pushd $WORK_DIR
+
+if [ -d "./.git" ]; then
+    echo "Clean out extra files"
+    git clean -fdx
+fi
+
+popd
+pushd $TMP_DIR
+
+# Tar it up.
+echo "Building tarball"
+if [ ! -d "$DEST_DIR" ]; then
+    mkdir -p "$DEST_DIR"
+fi
+TARFILE="${DEST_DIR}/ushahidi-platform-bundle-${VERSION}.tar"
+
+tar -cf $TARFILE \
+    --exclude 'build' \
+    --exclude 'docs' \
+    --exclude 'storage/logs/*' \
+    --exclude '.travis.yml' \
+    --exclude '.vagrant' \
+    --exclude 'tmp' \
+    --exclude '.env' \
+    --exclude '.env.travis' \
+    --exclude '.git' \
+    --exclude '.gitbook' \
+    ushahidi-platform-bundle-${VERSION}/
+
+gzip -f $TARFILE
+echo "Release tarball: ${TARFILE}.gz"
+
+ls -la ${TARFILE}.gz
+
+popd
+
+rm -rf $TMP_DIR
