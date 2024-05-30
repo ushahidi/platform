f4198a3f053ac5211a4c14f7212cb2251fecf315
Mh-Asmi <mnaasmi@gmail.com>
2024-05-31 01:41:43 +0400

    update poste create to accept whatsapp posts

--- src/Ushahidi/Modules/V5/Actions/Contact/Commands/CreateContactCommand.php
+++ src/Ushahidi/Modules/V5/Actions/Contact/Commands/CreateContactCommand.php
@@ -8,6 +8,8 @@ use Ushahidi\Modules\V5\Requests\ContactRequest;
 use Illuminate\Support\Facades\Auth;
 use Ushahidi\Core\Entity\Contact as ContactEntity;
 use Ushahidi\Modules\V5\Models\Stage;
+use Ushahidi\Contracts\Sources;
+use Ushahidi\Contracts\Contact as ContractContact;
 
 class CreateContactCommand implements Command
 {
@@ -33,6 +35,21 @@ class CreateContactCommand implements Command
         $input['type'] = $request->input('type');
         $input['contact'] = $request->input('contact');
         $input['can_notify'] = $request->input('can_notify');
+        $input['created'] = time();
+        $input['updated'] = null;
+
+        return new self(new ContactEntity($input));
+    }
+
+    public static function forWhatsapp($user_id, String $contact_number, String $type = "phone", $can_notify = 0): self
+    {
+
+        $user = Auth::user();
+        $input['user_id'] =$user_id ?? ($user ? $user->id : null);
+        $input['data_source'] = Sources::WHATSAPP;
+        $input['type'] = $type ?? ContractContact::PHONE;
+        $input['contact'] = $contact_number;
+        $input['can_notify'] = $can_notify ?? 0;
         $input['created'] = time();
         $input['updated'] = null;
 
