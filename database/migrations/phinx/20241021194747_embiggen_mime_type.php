84c9c8f07c1e59774339c2aaf164c7cc32743d09
ushahidlee <lee@ushahidi.com>
2024-10-21 22:05:14 +0200

    added db migration for mime type

---
+++ database/migrations/phinx/20241021194747_embiggen_mime_type.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class EmbiggenMimeType extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('media')
+            ->changeColumn('mime', 'string', ['limit' => 128])
+            ->update();
+    }
+
+    public function down()
+    {
+
+    }
+}
